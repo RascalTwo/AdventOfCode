16a93898728272128375aad8aa6d5b427596ef32
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-04 00:54:31 -0500

    Optimize 2020 Day 4

--- 2020/solutions/4/solve.py
+++ 2020/solutions/4/solve.py
@@ -1,31 +1,15 @@
 import os
+from typing import Callable, Tuple
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
-req_fields = [
-    'byr',
-    'iyr',
-    'eyr',
-    'hgt',
-    'hcl',
-    'ecl',
-    'pid',
-    #'cid',
-]
-def solve_one(data: str):
-	passports = data.split('\n\n')
-	valid = 0
-	for passport in passports:
-		bad = False
-		for field in req_fields:
-			if not field in passport:
-				bad = True
-				break
-		if not bad:
-			valid += 1
-	return valid
 
+REQUIRED_FIELDS = set(['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid',])
+
+
+def solve_one(data: str):
+	return sum(all(field in passport for field in REQUIRED_FIELDS) for passport in data.split('\n\n'))
 
 
 def test_one():
@@ -45,61 +29,101 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in''') == 2
 	print(solve_one(data))
-test_one()
+
+
+def is_int_in_range(
+	value: str,
+	precheck: Callable[[str], bool],
+	get_range: Callable[[str], Tuple[int, int]],
+	parse_int: Callable[[str], int] = int,
+) -> bool:
+	try:
+		num = parse_int(value)
+		if not precheck(value):
+			return False
+
+		low, high = get_range(value)
+		return low <= num <= high
+	except:
+		return False
+
+
+YEAR_RANGES = {
+	'byr': (1920, 2002),
+	'iyr': (2010, 2020),
+	'eyr': (2020, 2030)
+}
+
 
 def is_valid(field: str, value: str):
-	if field == 'byr':
-		if len(value) != 4:
-			return False
-		num = int(value)
-		return num >= 1920 and num <= 2002
-	if field == 'iyr':
-		if len(value) != 4:
-			return False
-		num = int(value)
-		return num >= 2010 and num <= 2020
-	if field == 'eyr':
-		if len(value) != 4:
-			return False
-		num = int(value)
-		return num >= 2020 and num <= 2030
-	if field == 'hgt':
-		if not value.endswith('in') and not value.endswith('cm'):
-			return False
-		num = int(value[:-2])
-		if value.endswith('cm'):
-			return num >= 150 and num <= 193
-		return num >= 59 and num <= 76
-	if field == 'hcl':
+	if not value:
+		return False
+
+	if field in YEAR_RANGES:
+		return is_int_in_range(
+			value,
+			lambda value: len(value) == 4,
+			lambda _: YEAR_RANGES[field]
+		)
+	elif field == 'hgt':
+		return is_int_in_range(
+			value,
+			lambda value: any(value.endswith(suffix) for suffix in ('in', 'cm')),
+			lambda value: (150, 193) if value.endswith('cm') else (59, 76),
+			lambda value: int(value[:-2])
+		)
+	elif field == 'hcl':
 		if value[0] != '#':
 			return False
-		valid_hcl_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
-		return all(char in valid_hcl_chars for char in value[1:])
-	if field == 'ecl':
-		return value in ['amb', 'blu', "brn", 'gry', 'grn', 'hzl', 'oth']
-	if field == 'pid':
+		if any(char.isupper() for char in value):
+			return False
+		try:
+			int(value[1:], 16)
+			return True
+		except:
+			return False
+	elif field == 'ecl':
+		return value in ('amb', 'blu', "brn", 'gry', 'grn', 'hzl', 'oth')
+	elif field == 'pid':
+		if len(value) != 9:
+			return False
 		try:
 			int(value)
-			return len(value) == 9
+			return True
 		except:
 			return False
 
+
+def test_is_valid():
+	assert is_valid('byr', '2002')
+	assert not is_valid('byr', '2003')
+
+	assert is_valid('hgt', '60in')
+	assert is_valid('hgt', '190cm')
+	assert not is_valid('hgt', '190in')
+	assert not is_valid('hgt', '190')
+
+	assert is_valid('hcl', '#123abc')
+	assert not is_valid('hcl', '#123abC')
+	assert not is_valid('hcl', '#123abz')
+	assert not is_valid('hcl', '123abc')
+
+	assert is_valid('ecl', 'brn')
+	assert not is_valid('ecl', 'wat')
+
+	assert is_valid('pid', '000000001')
+	assert not is_valid('pid', '0123456789')
+
+
 def solve_two(data: str):
-	passports = data.split('\n\n')
-	valid = 0
-	for passport in passports:
-		bad = False
-		for field in req_fields:
-			if not field in passport:
-				bad = True
-				break
-			value = passport.split(field + ':')[1].split(' ')[0].split()[0]
-			if not is_valid(field, value):
-				bad = True
-				break
-		if not bad:
-			valid += 1
-	return valid
+	return sum(
+		all(
+			field in passport
+			and is_valid(field, passport.split(field + ':')[1].split(' ')[0].split()[0].strip())
+			for field in REQUIRED_FIELDS
+		)
+		for passport in data.split('\n\n')
+	)
 
 
 def test_two():
@@ -131,4 +155,3 @@ eyr:2022
 
 iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719''') == 4
 	print(solve_two(data))
-test_two()
