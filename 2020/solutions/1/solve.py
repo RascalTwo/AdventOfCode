ad838cf2378dca86f2b095d97fcbbc4bf00558f2
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-06 19:56:26 -0500

    Add test scaffolding for 2020 Day 1

--- 2020/solutions/1/solve.py
+++ 2020/solutions/1/solve.py
@@ -1,23 +1,25 @@
+import os
 import itertools
 import math
 from typing import Optional
+
+
+DIRPATH = os.path.dirname(os.path.abspath(__file__))
+
 
 def solve(data: str, length: int) -> Optional[int]:
 	for items in itertools.combinations(map(int, data.split('\n')), length):
 		if sum(items) == 2020:
 			return math.prod(items)
 
+
 def solve_one(data: str):
 	return solve(data, 2)
 
-def solve_two(data: str):
-	return solve(data, 3)
-
-
-with open('input.in') as input_file:
-	data = input_file.read()
 
 def test_one():
+	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
+		data = input_file.read()
 	assert solve_one('''1721
 979
 366
@@ -26,7 +28,14 @@ def test_one():
 1456''') == 514579
 	print(solve_one(data))
 
+
+def solve_two(data: str):
+	return solve(data, 3)
+
+
 def test_two():
+	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
+		data = input_file.read()
 	assert solve_two('''1721
 979
 366
