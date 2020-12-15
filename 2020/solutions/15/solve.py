310903a85a5ed51eec4db5d56f3fe7c8fa1ef511
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-15 00:39:23 -0500

    Optimize Day 15

--- 2020/solutions/15/solve.py
+++ 2020/solutions/15/solve.py
@@ -1,43 +1,42 @@
 import os
-import re
-import math
-import itertools
 import collections
 
-from typing import Dict, List, Tuple
+from typing import DefaultDict, List
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
+def solve(data: str, target: int) -> int:
+	def speak(value: int):
+		nonlocal last_spoken
+		last_spoken = value
+		nums_to_turns[value].append(turn)
+
+	starting = list(map(int, data.split(',')))
+
+	last_spoken = 0
+	nums_to_turns: DefaultDict[int, List[int]] = collections.defaultdict(list)
+
+	turn = 0
+	while turn != target:
+		turn += 1
+		if turn <= len(starting):
+			speak(starting[turn-1])
+			continue
+
+		if len(nums_to_turns[last_spoken]) == 1:
+			speak(0)
+			continue
+
+		latest, second_latest = nums_to_turns[last_spoken][-2:][::-1]
+		speak(latest - second_latest)
+
+	return last_spoken
 
 
 def solve_one(data: str):
-	starting = list(map(int, data.split(',')))
-	turns_to_nums = {}
-	nums_to_turns = collections.defaultdict(lambda: [])
-	turn = -1
-	while turn != 2020:
-		turn += 1
-		if turn < len(starting):
-			speaking = starting[turn]
-			turns_to_nums[turn] = speaking
-			nums_to_turns[speaking].append(turn)
-			continue
-
-		last_spoken = turns_to_nums[turn-1]
-		if len(nums_to_turns[last_spoken]) == 1:
-			speaking = 0
-			turns_to_nums[turn] = speaking
-			nums_to_turns[speaking].append(turn)
-			continue
-
-		latest, second_latest = list(map(lambda n: n + 1, nums_to_turns[last_spoken][-2:][::-1]))
-		speaking = latest - second_latest
-		turns_to_nums[turn] = speaking
-		nums_to_turns[speaking].append(turn)
-
-	return turns_to_nums[2019]
+	return solve(data, 2020)
 
 
 def test_one():
@@ -48,31 +47,7 @@ def test_one():
 
 
 def solve_two(data: str):
-	starting = list(map(int, data.split(',')))
-	turns_to_nums = {}
-	nums_to_turns = collections.defaultdict(lambda: [])
-	turn = -1
-	while turn != 30000000:
-		turn += 1
-		if turn < len(starting):
-			speaking = starting[turn]
-			turns_to_nums[turn] = speaking
-			nums_to_turns[speaking].append(turn)
-			continue
-
-		last_spoken = turns_to_nums[turn-1]
-		if len(nums_to_turns[last_spoken]) == 1:
-			speaking = 0
-			turns_to_nums[turn] = speaking
-			nums_to_turns[speaking].append(turn)
-			continue
-
-		latest, second_latest = list(map(lambda n: n + 1, nums_to_turns[last_spoken][-2:][::-1]))
-		speaking = latest - second_latest
-		turns_to_nums[turn] = speaking
-		nums_to_turns[speaking].append(turn)
-
-	return turns_to_nums[30000000-1]
+	return solve(data, 30000000)
 
 
 def test_two():
