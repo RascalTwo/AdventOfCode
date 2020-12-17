ba905dcc4bbf243708a0cae2afbac05e12792e64
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-17 01:12:32 -0500

    Optimize Day 17

--- 2020/solutions/17/solve.py
+++ 2020/solutions/17/solve.py
@@ -1,70 +1,56 @@
 import os
-import re
-import math
 import itertools
-import collections
 
-from typing import Dict, List, Tuple, Set
+from typing import Dict, Iterator, List, Tuple, cast
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
-Point = Tuple[int, int, int]
 
-def get_neighbor_states(world: Dict[Point, str], z: int, x: int, y: int) -> List[str]:
-	#orig_sum = x + z + y
-	neighbors = []
-	for zd in (-1, 0, 1):
-		for xd in (-1, 0, 1):
-			for yd in (-1, 0, 1):
-				nz = z + zd
-				nx = x + xd
-				ny = y + yd
-				if nx == x and ny == y and nz == z:
-					continue
-				neighbors.append(world.get((nz, nx, ny), '.'))
-				#new_sum = nz + nx + ny
-				#diff = abs(new_sum - orig_sum)
-				#if diff == 1:
-					#continue
+Point = Tuple[int, ...]
 
-	return neighbors
 
-def solve_one(data: str):
-	world: Dict[Point, str] = {}
-	matrix: List[List[str]] = list(map(list, data.split('\n')))
-	for x, row in enumerate(matrix):
-		for y, col in enumerate(row):
-			world[(0, x, y)] = col
+def get_neighbor_coords(point: Point) -> Iterator[Point]:
+	return (
+		new_point
+		for offset in itertools.product((-1, 0, 1), repeat=len(point))
+		if (new_point := tuple(point[i] + offset[i] for i in range(len(point)))) != point
+	)
+
+
+def parse_input(data: str, dimensions: int) -> Dict[Point, str]:
+	return {
+		(*(0, ) * (dimensions - 2), x, y): col
+		for x, row in enumerate(cast(List[List[str]], list(map(list, data.split('\n')))))  # type: ignore
+		for y, col in enumerate(row)
+	}
+
+
+def solve(data: str, dimensions: int):
+	world = parse_input(data, dimensions)
 
 	for _ in range(6):
-		values = [[point[i] for point in world.keys()] for i in range(3)]
 		next_world = world.copy()
-		for z in range(min(values[0]) - 1, max(values[0]) + 2):
-			for x in range(min(values[1]) - 1, max(values[1]) + 2):
-				for y in range(min(values[2]) - 1, max(values[2]) + 2):
-					neighbors = get_neighbor_states(world, z, x, y)
-					active_n = neighbors.count('#')
-					point = (z, x, y)
-					current = world.get(point, '.')
-					if current == '#':
-						if active_n in (2, 3):
-							next_world[point] = '#'
-						else:
-							next_world[point] = '.'
-					else:
-						if active_n == 3:
-							next_world[point] = '#'
-						else:
-							next_world[point] = '.'
+
+		values = [[point[i] for point in world.keys()] for i in range(dimensions)]
+		for point in itertools.product(
+			*(range(min(values[i]) - 1, max(values[i]) + 2) for i in range(dimensions))
+		):
+			active = sum(world.get(neighbor, '.') == '#' for neighbor in get_neighbor_coords(point))
+			current = world.get(point, '.')
+			if current == '#' and active not in (2, 3):
+				next_world[point] = '.'
+			elif current == '.' and active == 3:
+				next_world[point] = '#'
+
 		world = next_world
 
-	ac = 0
-	for value in world.values():
-		ac += value == '#'
-	return ac
+	return sum(value == '#' for value in world.values())
 
+
+def solve_one(data: str):
+	return solve(data, 3)
 
 
 def test_one():
@@ -76,62 +62,8 @@ def test_one():
 	print(solve_one(data))
 
 
-def get_neighbor_states_two(world: Dict[Point, str], w: int, z: int, x: int, y: int) -> List[str]:
-	#orig_sum = x + z + y
-	neighbors = []
-	for wd in (-1, 0, 1):
-		for zd in (-1, 0, 1):
-			for xd in (-1, 0, 1):
-				for yd in (-1, 0, 1):
-					nw = w + wd
-					nz = z + zd
-					nx = x + xd
-					ny = y + yd
-					if nw == w and nz == z and nx == x and ny == y:
-						continue
-					neighbors.append(world.get((nw, nz, nx, ny), '.'))
-					#new_sum = nz + nx + ny
-					#diff = abs(new_sum - orig_sum)
-					#if diff == 1:
-						#continue
-
-	return neighbors
-
 def solve_two(data: str):
-	world: Dict[Point, str] = {}
-	matrix: List[List[str]] = list(map(list, data.split('\n')))
-	for x, row in enumerate(matrix):
-		for y, col in enumerate(row):
-			world[(0, 0, x, y)] = col
-
-	for _ in range(6):
-		values = [[point[i] for point in world.keys()] for i in range(4)]
-		next_world = world.copy()
-
-		for w in range(min(values[0]) - 1, max(values[0]) + 2):
-			for z in range(min(values[1]) - 1, max(values[1]) + 2):
-				for x in range(min(values[2]) - 1, max(values[2]) + 2):
-					for y in range(min(values[3]) - 1, max(values[3]) + 2):
-						neighbors = get_neighbor_states_two(world, w, z, x, y)
-						active_n = neighbors.count('#')
-						point = (w, z, x, y)
-						current = world.get(point, '.')
-						if current == '#':
-							if active_n in (2, 3):
-								next_world[point] = '#'
-							else:
-								next_world[point] = '.'
-						else:
-							if active_n == 3:
-								next_world[point] = '#'
-							else:
-								next_world[point] = '.'
-		world = next_world
-
-	ac = 0
-	for value in world.values():
-		ac += value == '#'
-	return ac
+	return solve(data, 4)
 
 
 def test_two():
