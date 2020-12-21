90352a3d5e2151006e2587328450e708d96f78da
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-21 01:34:05 -0500

    Optimize Day 21

--- 2020/solutions/21/solve.py
+++ 2020/solutions/21/solve.py
@@ -1,35 +1,37 @@
 import os
-import re
-import math
-import itertools
 import collections
 
-from typing import Dict, List, Tuple
+from typing import DefaultDict, Dict, Iterator, Set, Tuple
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def solve_one(data: str):
-	all_ings = set()
-	ing_count = collections.defaultdict(lambda: 0)
-	alg_ing = {}
-	for line in data.strip().split('\n'):
-		contains = list(map(lambda p: p.strip(), line.split('(')[1].split(')')[0].split('contains ')[1].split(',')))
-		ingredients = set(line.split('(')[0].strip().split(' '))
-		all_ings |= ingredients
-		for ing in ingredients:
-			ing_count[ing] +=1
+def parse_input(data: str) -> Iterator[Tuple[Set[str], Set[str]]]:
+	for line in data.split('\n'):
+		ingredients, allergens = line[:-1].split(' (contains ')
+		yield set(ingredients.split(' ')), set(allergens.split(', '))
 
-		for alg in contains:
-			alg_ing[alg] = alg_ing.get(alg, ingredients.copy()).intersection(ingredients)
-	bad = all_ings
-	for alg, ings in alg_ing.items():
-		for ing in ings:
-			if ing in bad:
-				bad.remove(ing)
-	return sum(ing_count[alg] for alg in bad)
+
+def solve_one(data: str):
+	ingredient_counts: DefaultDict[str, int] = collections.defaultdict(lambda: 0)
+	allergic_ingredients: Dict[str, Set[str]] = {}
+	for ingredients, allergens in parse_input(data):
+		ingredient_counts.update({
+			ingredient: ingredient_counts[ingredient] + 1
+			for ingredient in ingredients
+		})
+		allergic_ingredients.update({
+			allergen: allergic_ingredients.get(allergen, ingredients.copy()).intersection(ingredients)
+			for allergen in allergens
+		})
+
+	return sum(ingredient_counts[safe] for safe in [
+		ingredient
+		for ingredient in ingredient_counts.keys()
+		if all(ingredient not in allergens for allergens in allergic_ingredients.values())
+	])
 
 
 def test_one():
@@ -38,43 +40,46 @@ def test_one():
 	assert solve_one('''mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
-sqjhc mxmxvkd sbzzf (contains fish)
-''') == 5
+sqjhc mxmxvkd sbzzf (contains fish)''') == 5
 	print(solve_one(data))
 
 
 def solve_two(data: str):
-	all_ings = set()
-	ing_count = collections.defaultdict(lambda: 0)
-	alg_ing = {}
-	for line in data.strip().split('\n'):
-		contains = list(map(lambda p: p.strip(), line.split('(')[1].split(')')[0].split('contains ')[1].split(',')))
-		ingredients = set(line.split('(')[0].strip().split(' '))
-		all_ings |= ingredients
-		for ing in ingredients:
-			ing_count[ing] +=1
+	ingredient_counts: DefaultDict[str, int] = collections.defaultdict(lambda: 0)
+	allergic_ingredients: Dict[str, Set[str]] = {}
+	for ingredients, allergens in parse_input(data):
+		ingredient_counts.update({
+			ingredient: ingredient_counts[ingredient] + 1
+			for ingredient in ingredients
+		})
+		allergic_ingredients.update({
+			allergen: allergic_ingredients.get(allergen, ingredients.copy()).intersection(ingredients)
+			for allergen in allergens
+		})
 
-		for alg in contains:
-			alg_ing[alg] = alg_ing.get(alg, ingredients.copy()).intersection(ingredients)
 
-	bad = all_ings
-	for alg, ings in alg_ing.items():
-		for ing in ings:
-			if ing in bad:
-				bad.remove(ing)
+	allergic_ingredient_lists = {
+		allergen: list(ingredients)
+		for allergen, ingredients in allergic_ingredients.items()
+	}
 
-	algerns = sorted(alg_ing.items(), key=lambda pair: len(pair[1]))
-	for alg, ings in algerns:
-		if len(ings) != 1:
-			continue
-		ing = next(iter(ings))
-		for oalt, oings in algerns:
-			if alg == oalt:
+	processing = True
+	while processing:
+		processing = False
+		for allergen, ingredients in sorted(allergic_ingredient_lists.items(), key=lambda pair: len(pair[1])):
+			if len(ingredients) != 1:
 				continue
-			if ing in oings:
-				oings.remove(ing)
+			ingredient = ingredients[0]
+			for other_allergen, other_ingredients in allergic_ingredient_lists.items():
+				if allergen == other_allergen or ingredient not in other_ingredients:
+					continue
+				other_ingredients.remove(ingredient)
+				processing = True
 
-	return ','.join(next(iter(ings)) for _, ings in sorted(algerns, key=lambda pair: pair[0]))
+	return ','.join(
+		ingredients[0]
+		for _, ingredients in sorted(allergic_ingredient_lists.items(), key=lambda pair: pair[0])
+	)
 
 def test_two():
 	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
@@ -82,6 +87,5 @@ def test_two():
 	assert solve_two('''mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
 trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
-sqjhc mxmxvkd sbzzf (contains fish)
-''') == 'mxmxvkd,sqjhc,fvjkl'
+sqjhc mxmxvkd sbzzf (contains fish)''') == 'mxmxvkd,sqjhc,fvjkl'
 	print(solve_two(data))
