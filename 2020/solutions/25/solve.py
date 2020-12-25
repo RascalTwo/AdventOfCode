ac5d1c0753ad6689c5a5ac5dc52c9795a00d1a35
Rascal_Two <therealrascaltwo@gmail.com>
2020-12-25 01:11:40 -0500

    Optimize Day 25

--- 2020/solutions/25/solve.py
+++ 2020/solutions/25/solve.py
@@ -1,27 +1,19 @@
 import os
-import re
-import math
-import itertools
-import collections
-
-from typing import Dict, List, Tuple
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def find_loop_num(subject: int, target: int, mod: int) -> int:
-	for loop in range(1000000000):
-		if pow(subject, loop, mod) == target:
-			return loop
-
-
 def solve_one(data: str):
 	card, door = list(map(int, data.split('\n')))
-	dl = find_loop_num(7, door, 20201227)
 
-	return pow(card, dl, 20201227)
+	loop = 1
+	while True:
+		if pow(7, loop, 20201227) == door:
+			return pow(card, loop, 20201227)
+		loop += 1
+
 
 def test_one():
 	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
