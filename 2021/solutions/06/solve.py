2d1fbadb4cded189116449fda9fcef89a763bd31
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-06 00:19:02 -0500

    Optimize 2021 Day 06 Python solution

--- 2021/solutions/06/solve.py
+++ 2021/solutions/06/solve.py
@@ -1,52 +1,39 @@
 import os
 import collections
 
+from typing import Counter
+
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
 
-def solve_one(data: str):
-	ages = list(map(int, data.strip().split(',')))
-	fishes = []
-	for age in ages:
-		fishes.append({ 'age': age })
+def solve(data: str, days: int):
+	counts = collections.Counter(map(int, data.strip().split(',')))
 
-	for _ in range(80):
-		for fish in fishes:
-			fish['age'] -= 1
-			if fish['age'] < 0:
-				fishes.append({ 'age': 9 })
-				fish['age'] = 6
+	for _ in range(days):
+		new_counts: Counter[int] = collections.Counter()
 
-	return len(fishes)
+		for age, count in counts.items():
+			if age:
+				new_counts[age - 1] += count
+				continue
+			new_counts[6] += count
+			new_counts[8] += count
+
+		counts = new_counts
+
+	return sum(counts.values())
 
 
 def test_one():
 	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
 		data = input_file.read()
-	assert solve_one('''3,4,3,1,2''') == 5934
-	print(solve_one(data))
-
-
-def solve_two(data: str):
-	counts = collections.Counter()
-	for fish in map(int, data.strip().split(',')):
-		counts[fish] += 1
-
-	for _ in range(256):
-		new_counts = collections.Counter()
-		for age, v in counts.items():
-			if age:
-				new_counts[age - 1] += v
-			else:
-				new_counts[6] += v
-				new_counts[8] += v
-		counts = new_counts
-	return sum(counts.values())
+	assert solve('3,4,3,1,2', 80) == 5934
+	print(solve(data, 80))
 
 
 def test_two():
 	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
 		data = input_file.read()
-	assert solve_two('''3,4,3,1,2''') == 26984457539
-	print(solve_two(data))
+	assert solve('3,4,3,1,2', 256) == 26984457539
+	print(solve(data, 256))
