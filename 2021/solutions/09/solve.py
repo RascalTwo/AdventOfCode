8472ecb2aa77b75004ad2cad21a47a2854c5d3b0
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-09 00:33:58 -0500

    Optimize 2021 Day 09 Python solution

--- 2021/solutions/09/solve.py
+++ 2021/solutions/09/solve.py
@@ -1,33 +1,38 @@
 import os
+import math
 
-from typing import List, Tuple
+from typing import Dict, List, Set, Tuple
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
+Matrix = Dict[complex, int]
+
+
+def parse_matrix(data: str) -> Matrix:
+	return {
+		complex(r, c): int(col)
+		for r, row in enumerate(data.strip().split('\n'))
+		for c, col in enumerate(row)
+	}
+
+
+def is_low(matrix: Matrix, coordinate: complex) -> bool:
+	current = matrix[coordinate]
+
+	for offset in (complex(0, 1), complex(0, -1), complex(-1, 0), complex(1, 0)):
+		trying = coordinate + offset
+		if trying in matrix and current >= matrix[trying]:
+			return False
+
+	return True
+
+
 def solve_one(data: str):
-	matrix = []
-	for line in data.strip().split('\n'):
-		matrix.append(list(map(int, list(line))))
-	def is_low(r: int, c: int) -> bool:
-		current = matrix[r][c]
-		offsets = [(0, 1), (0, -1), (-1, 0), (1, 0)]
-		for ro, co in offsets:
-			nr, nc = r + ro, c + co
-			if nr < 0 or nc < 0 or nr >= len(matrix) or nc >= len(matrix[nr]):
-				continue
-			other = matrix[nr][nc]
-			if current >= other:
-				return False
-		return True
-	risk = 0
-	for r, row in enumerate(matrix):
-		for c, col in enumerate(row):
-			if is_low(r, c):
-				risk += 1 + col
-	return risk
+	matrix = parse_matrix(data)
+	return sum(1 + matrix[coordinate] for coordinate in matrix if is_low(matrix, coordinate))
 
 
 def test_one():
@@ -41,54 +46,27 @@ def test_one():
 	print(solve_one(data))
 
 
-def solve_two(data: str):
-	matrix = []
-	for line in data.strip().split('\n'):
-		matrix.append(list(map(int, list(line))))
-	def is_low(r: int, c: int) -> bool:
-		current = matrix[r][c]
-		offsets = [(0, 1), (0, -1), (-1, 0), (1, 0)]
-		for ro, co in offsets:
-			nr, nc = r + ro, c + co
-			if nr < 0 or nc < 0 or nr >= len(matrix) or nc >= len(matrix[nr]):
-				continue
-			other = matrix[nr][nc]
-			if current >= other:
-				return False
-		return True
-	lowpoints = []
-	for r, row in enumerate(matrix):
-		for c, col in enumerate(row):
-			if is_low(r, c):
-				lowpoints.append((r, c))
-	def find_basin(lr: int, lc: int) -> List[Tuple[int, int]]:
-		basin = []
-		processing = [(lr, lc)]
-		while processing:
-			r, c = processing.pop()
-			basin.append((r, c))
-			for ro, co in [(0, 1), (0, -1), (-1, 0), (1, 0)]:
-				nr, nc = r + ro, c + co
-				if (nr, nc) in basin:
-					continue
-				if nr < 0 or nc < 0 or nr >= len(matrix) or nc >= len(matrix[nr]):
-					continue
-				other = matrix[nr][nc]
-				if other < 9:
-					processing.append((nr, nc))
-		return set(basin)
+def flood_basin(matrix: Matrix, basin: List[complex]) -> int:
+	coordinate = basin[-1]
+	for offset in (complex(0, 1), complex(0, -1), complex(-1, 0), complex(1, 0)):
+		trying = coordinate + offset
+		if trying not in basin and trying in matrix and matrix[trying] < 9:
+			basin.append(trying)
+			flood_basin(matrix, basin)
 
-	basins = []
-	for lowpoint in lowpoints:
-		basins.append(find_basin(*lowpoint))
-	basins.sort(key = lambda basin: len(basin), reverse=True)
-	result = None
-	for basin in basins[:3]:
-		if result is None:
-			result = len(basin)
-		else:
-			result *= len(basin)
-	return result
+	return len(basin)
+
+
+def solve_two(data: str):
+	matrix = parse_matrix(data)
+	return math.prod(sorted((
+		flood_basin(matrix, [low_point])
+		for low_point in (
+			coordinate
+			for coordinate in matrix
+			if is_low(matrix, coordinate)
+		)
+	), reverse=True)[:3])
 
 
 def test_two():
