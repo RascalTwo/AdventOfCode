ac7a9ea5fae5adf579d3740f23902da6f29d42a2
Rascal_Two <therealrascaltwo@gmail.com>
2021-12-13 00:44:04 -0500

    Optimize 2021 Day 13 Python solution

--- 2021/solutions/13/solve.py
+++ 2021/solutions/13/solve.py
@@ -1,47 +1,46 @@
 import os
-import re
-import math
-import itertools
-import collections
 
-from typing import Dict, List, Tuple
+from typing import Set, Tuple
 
 
 
 DIRPATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def solve_one(data: str):
-	rpositions, rfolds = data.strip().split('\n\n')
-	flines = rfolds.split('\n')
-	dlines = rpositions.split('\n')
-	dots = set()
-	for dline in dlines:
-		x, y = dline.split(',')
-		dots.add((int(x), int(y)))
-	folds = [fold.split('along ')[1].split('=') for fold in flines]
+def solve(data: str, all_folds: bool):
+	dot_lines, fold_lines = data.strip().split('\n\n')
+	dots: Set[Tuple[int, int]] = set(tuple(map(int, pair.split(','))) for pair in dot_lines.split('\n'))  # type: ignore
+	folds = [(axis, int(loc)) for axis, loc in [fold.split('along ')[1].split('=') for fold in fold_lines.split('\n')]]
 
-	for axis, line in folds[:1]:
-		newdots = set()
-		line = int(line)
-		if axis == 'x':
-			x = line
-			for pos in dots:
-				if pos[0] < x:
-					newdots.add(pos)
-				else:
-					newdots.add((2 * x - pos[0], pos[1]))
-		else:
-			y = line
-			for pos in dots:
-				if pos[1] < y:
-					newdots.add(pos)
-				else:
-					newdots.add((pos[0], 2 * y - pos[1]))
-		dots = newdots
+	for axis, line in folds if all_folds else folds[:1]:
+		index = 'xy'.index(axis)
+
+		new_dots: Set[Tuple[int, int]] = set()
+		for dot in dots:
+			if dot[index] < line:
+				new_dots.add(dot)
+			else:
+				offset = 2 * line
+				new_dots.add((offset - dot[0], dot[1]) if index == 0 else (dot[0], offset - dot[1]))
+		dots = new_dots
+
+	if all_folds:
+		mx = 1
+		my = 1
+		for dot in dots:
+			mx = max(mx, dot[0])
+			my = max(my, dot[1])
+
+		for y in range(my + 1):
+			for x in range(mx + 1):
+				print(' #'[(x, y) in dots], end='', flush=True)
+			print('')
 
 	return len(dots)
 
+
+def solve_one(data: str):
+	return solve(data, False)
 
 
 def test_one():
@@ -72,51 +71,13 @@ fold along x=5''') == 17
 
 
 def solve_two(data: str):
-	rpositions, rfolds = data.strip().split('\n\n')
-	flines = rfolds.split('\n')
-	dlines = rpositions.split('\n')
-	dots = set()
-	for dline in dlines:
-		x, y = dline.split(',')
-		dots.add((int(x), int(y)))
-	folds = [fold.split('along ')[1].split('=') for fold in flines]
-
-	for axis, line in folds:
-		newdots = set()
-		line = int(line)
-		if axis == 'x':
-			x = line
-			for pos in dots:
-				if pos[0] < x:
-					newdots.add(pos)
-				else:
-					newdots.add((2 * x - pos[0], pos[1]))
-		else:
-			y = line
-			for pos in dots:
-				if pos[1] < y:
-					newdots.add(pos)
-				else:
-					newdots.add((pos[0], 2 * y - pos[1]))
-		dots = newdots
-
-	width = 0
-	height = 0
-	for dot in dots:
-		width = max(width, dot[0])
-		height = max(height, dot[1])
-	
-	for y in range(height+1):
-		for x in range(width+1):
-			print('.' if (x, y) not in dots else '#', end='', flush=True)
-		print('')
-
+	return solve(data, True)
 
 
 def test_two():
 	with open(os.path.join(DIRPATH, 'input.in')) as input_file:
 		data = input_file.read()
-	solve_two('''6,10
+	assert solve_two('''6,10
 0,14
 9,10
 0,3
@@ -136,5 +97,5 @@ def test_two():
 9,0
 
 fold along y=7
-fold along x=5''')
+fold along x=5''') == 16
 	solve_two(data)
