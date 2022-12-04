e2467847f3fe62d6551860e5aa63ec0389c3a7d6
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-04 00:33:09 -0500

    Optimize 2022 Day 04 TypeScript solution

--- 2022/solutions/04/solve.ts
+++ 2022/solutions/04/solve.ts
@@ -2,16 +2,13 @@ const fs = require('fs');
 const assert = require('assert');
 
 
+const parseSectionRanges = (data: string): [[number, number], [number, number]][] => data.split('\n').map(line =>
+		line.split(',').map(p => p.split('-').map(Number) as [number ,number]) as [[number, number], [number, number]])
 
 function solveOne(data: string): any{
-	return data.split('\n').reduce((sum, thing) => {
-		const [e1, e2] = thing.split(',').map(p => p.split('-').map(Number));
-		// check if e2 is within e1
-		if (e1[0] <= e2[0] && e1[1] >= e2[1]) return sum + 1;
-		// check if e1 is within e2
-		if (e2[0] <= e1[0] && e2[1] >= e1[1]) return sum + 1;
-		return sum;
-	}, 0)
+	return parseSectionRanges(data).reduce((sum, [e1, e2]) => {
+		return sum + +(e1[0] <= e2[0] && e1[1] >= e2[1] || e2[0] <= e1[0] && e2[1] >= e1[1])
+	}, 0);
 }
 
 
@@ -28,18 +25,8 @@ function solveOne(data: string): any{
 
 
 function solveTwo(data: string): any{
-	return data.split('\n').reduce((sum, thing) => {
-		const [e1, e2] = thing.split(',').map(p => p.split('-').map(Number));
-		// generate set of numbers from e1[0] to e1[1]
-		const set1 = new Set();
-		for (let i = e1[0]; i <= e1[1]; i++) set1.add(i);
-		// generate set of numbers from e2[0] to e2[1]
-		const set2 = new Set();
-		for (let i = e2[0]; i <= e2[1]; i++) set2.add(i);
-
-		// if sets have intersection
-		if (new Set([...set1].filter(x => set2.has(x))).size > 0) return sum + 1;
-		return sum;
+	return parseSectionRanges(data).reduce((sum, [e1, e2]) => {
+		return sum + +((e1[0] <= e2[0] && e1[1] >= e2[1] || e2[0] <= e1[0] && e2[1] >= e1[1]) || (e1[0] <= e2[0] && e1[1] >= e2[0] || e2[0] <= e1[0] && e2[1] >= e1[0]))
 	}, 0)
 }
 
