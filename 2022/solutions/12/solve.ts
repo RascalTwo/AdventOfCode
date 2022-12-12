bcb6324cb7db7265015bde3630153df383b0c362
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-12 01:20:32 -0500

    Optimize 2022 Day 12 TypeScript solution

--- 2022/solutions/12/solve.ts
+++ 2022/solutions/12/solve.ts
@@ -2,87 +2,55 @@ const fs = require('fs');
 const assert = require('assert');
 
 
-function solveOne(data: string): any {
-	const matrix = data.split('\n').map(line => line.split('').map(char => char.charCodeAt(0) - 96));
-	let start = null;
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[i].length; j++) {
-			if (matrix[i][j] === -13) {
-				start = [i, j];
-				break;
-			}
-		}
-	}
-	matrix[start![0]][start![1]] = 1;
-	let end = null;
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[i].length; j++) {
-			if (matrix[i][j] === -27) {
-				end = [i, j];
-				break;
-			}
-		}
-	}
-	matrix[end![0]][end![1]] = 26;
+function bfs(matrix: number[][], end: [number, number], ...starts: [number, number][]): number {
+	const queue = starts.map(start => ({ coord: start, cost: 0 }))
+	const seen = new Map<number, Set<number>>()
+	while (queue[0].coord[0] !== end[0] || queue[0].coord[1] !== end[1]) {
+		const { coord: [x, y], cost } = queue.shift()!;
 
-	const visited = new Set();
-	const costs = new Map();
-	const parents = new Map();
-	const queue = new Map();
-	queue.set(start?.join(','), 0);
-	while (queue.size > 0) {
-		let min = Infinity;
-		let minKey = null;
-		for (const [key, value] of queue) {
-			if (value < min) {
-				min = value;
-				minKey = key;
-			}
-		}
-		const [x, y] = minKey.split(',').map(Number);
-		queue.delete(minKey);
-		visited.add(minKey);
-		if (minKey === end?.join(',')) {
-			break;
-		}
-		const neighbors = [
+		if (seen.get(x)?.has(y)) continue;
+		if (!seen.has(x)) seen.set(x, new Set());
+		seen.get(x)!.add(y);
+
+		for (const neighbor of [
 			[x - 1, y],
 			[x + 1, y],
 			[x, y - 1],
 			[x, y + 1],
-		].filter(([i, j]) => {
-			// if weight is greater then one higher then current weight, then it is not a neighbor
-			if (matrix[i]?.[j] > matrix[x][y] + 1) {
-				return false;
-			}
-			return true;
-		});
-		for (const neighbor of neighbors) {
+		]) {
 			const [i, j] = neighbor;
-			if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[i].length) {
-				continue;
-			}
-			if (visited.has(neighbor.join(','))) {
-				continue;
-			}
-			const cost = costs.get(minKey) || 0;
-			const newCost = cost + matrix[i][j];
-			const oldCost = costs.get(neighbor.join(','));
-			if (oldCost === undefined || newCost < oldCost) {
-				costs.set(neighbor.join(','), newCost);
-				parents.set(neighbor.join(','), minKey);
-				queue.set(neighbor.join(','), newCost);
+			if (!(i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] > matrix[x][y] + 1)) {
+				queue.push({ coord: [i, j], cost: cost + 1 })
 			}
 		}
 	}
-	let current = end?.join(',');
-	let path = [];
-	while (current !== start?.join(',')) {
-		path.push(current);
-		current = parents.get(current);
+
+	return queue[0].cost
+}
+
+function parseElevations(data: string) {
+	const matrix = data.split('\n').map(line => line.split('').map(char => char.charCodeAt(0) - 96));
+	let start: [number, number] | null = null;
+	let end: [number, number] | null = null;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix[i][j] === -13) start = [i, j];
+			if (matrix[i][j] === -27) end = [i, j];
+
+			if (end && start) break;
+		}
+		if (end && start) break;
 	}
 
-	return path.length;
+	matrix[start![0]][start![1]] = 1;
+	matrix[end![0]][end![1]] = 26;
+
+	return { matrix, start: start!, end: end! };
+}
+
+function solveOne(data: string): any {
+	const { matrix, start, end } = parseElevations(data)
+	return bfs(matrix, end, start);
 }
 
 
@@ -97,20 +65,10 @@ abdefghi`), 31);
 })();
 
 
-
 function solveTwo(data: string): any {
-	const matrix = data.split('\n').map(line => line.split('').map(char => char.charCodeAt(0) - 96));
-	let start = null;
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[i].length; j++) {
-			if (matrix[i][j] === -13) {
-				start = [i, j];
-				break;
-			}
-		}
-	}
-	matrix[start![0]][start![1]] = 1;
-	const starts = []
+	const { matrix, end } = parseElevations(data);
+
+	const starts: [number, number][] = []
 	for (let i = 0; i < matrix.length; i++) {
 		for (let j = 0; j < matrix[i].length; j++) {
 			if (matrix[i][j] === 1) {
@@ -118,64 +76,8 @@ function solveTwo(data: string): any {
 			}
 		}
 	}
-	let end = null;
-	for (let i = 0; i < matrix.length; i++) {
-		for (let j = 0; j < matrix[i].length; j++) {
-			if (matrix[i][j] === -27) {
-				end = [i, j];
-				break;
-			}
-		}
-	}
-	matrix[end![0]][end![1]] = 26;
-	// function to convert coordinate to single number based on matrix size
-	const coordToNum = (coord: [number, number]) => {
-		const [i, j] = coord;
-		return i * matrix[0].length + j;
-	}
-	// function to convert single number to coordinate based on matrix size
-	const numToCoord = (num: number) => {
-		const i = Math.floor(num / matrix[0].length);
-		const j = num % matrix[0].length;
-		return [i, j];
-	}
-	// @ts-ignore
-	//end = coordToNum(end!);
-	if (!end) return
 
-
-	const queue = starts.map(start => ({ coord: start, cost: 0 }))
-	const seen: Record<number, Record<number, true>> = {}
-	let best = Infinity;
-	while(queue[0].coord[0] !== end[0] || queue[0].coord[1] !== end[1]) {
-		const { coord: [x, y], cost } = queue.shift()!;
-		if (seen?.[x]?.[y]) {
-			continue;
-		}
-		seen[x] = seen[x] || {};
-		seen[x][y] = true;
-		const neighbors = [
-			[x - 1, y],
-			[x + 1, y],
-			[x, y - 1],
-			[x, y + 1],
-		].filter(([i, j]) => {
-			// if weight is greater then one higher then current weight, then it is not a neighbor
-			if (matrix[i]?.[j] > matrix[x][y] + 1) {
-				return false;
-			}
-			return true;
-		});
-		for (const neighbor of neighbors) {
-			const [i, j] = neighbor;
-			if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
-				continue;
-			}
-			queue.push({ coord: [i, j], cost: cost + 1 });
-		}
-	}
-
-	return queue[0].cost
+	return bfs(matrix, end!, ...starts);
 }
 
 
