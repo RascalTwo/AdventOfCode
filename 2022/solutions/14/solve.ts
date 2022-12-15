625d7690dc48ae659b2cc01c202c65f48d24d184
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-14 23:49:39 -0500

    Optimize 2022 Day 14 TypeScript solution

--- 2022/solutions/14/solve.ts
+++ 2022/solutions/14/solve.ts
@@ -3,59 +3,70 @@ const assert = require('assert');
 
 
 
-function solveOne(data: string): any {
-	const world = new Map();
+function placeWalls(data: string, floor = false): [Map<string, '#' | 'o'>, number] {
+	let greatestY = -Infinity;
+
+	const world = new Map<string, '#' | 'o'>();
 	for (const coordList of data.split('\n')) {
-		let [x, y] = coordList.split(' -> ')[0].split(',').map(Number);
-		for (const coord of coordList.split(' -> ').slice(1)) {
-			const [x2, y2] = coord.split(',').map(Number);
+		const coords = coordList.split(' -> ').map(rawCoord => rawCoord.split(',').map(Number));
+		let [x, y] = coords[0];
+		world.set(`${x},${y}`, '#');
+		for (const [x2, y2] of coords.slice(1)) {
+			if (y2 > greatestY) greatestY = y2;
 			while (x !== x2 || y !== y2) {
-				world.set(`${x},${y}`, (world.get(`${x},${y}`) || 0) + 1);
-				if (x !== x2) {
-					x += x < x2 ? 1 : -1;
-				}
-				if (y !== y2) {
-					y += y < y2 ? 1 : -1;
-				}
+				if (x !== x2) x += x < x2 ? 1 : -1;
+				else if (y !== y2) y += y < y2 ? 1 : -1;
+				world.set(`${x},${y}`, '#');
 			}
-			world.set(`${x},${y}`, (world.get(`${x},${y}`) || 0) + 1);
-		}
-	}
-	const greatestY = Math.max(...Array.from(world.keys()).map((key: string) => Number(key.split(',')[1])));
-	sandFalling:
-	while (true) {
-		let sand = [500, 0]
-		while (true) {
-			if (sand[1] > greatestY) break sandFalling;
-			if (world.get(`${sand[0]},${sand[1]}`) === undefined) {
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue
-			}
-			if (world.get(`${sand[0]},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			if (world.get(`${sand[0] - 1},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[0]--;
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			if (world.get(`${sand[0] + 1},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[0]++;
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			break;
 		}
 	}
 
-	return [...world.values()].filter((value: any) => value === 's').length - 1;
+	if (floor) {
+		greatestY += 2;
+		for (let x = -1000; x < 1000; x++) world.set(`${x},${greatestY}`, '#');
+	}
+
+	return [ world, greatestY ];
+}
+
+function simulateSand(world: Map<string, '#' | 'o'>, greatestY: number) {
+	sandFalling:
+	while (true) {
+		let [x, y] = [500, 0];
+		if (world.get(`${x},${y}`) === 'o') break;
+		while (true) {
+			if (y >= greatestY) {
+				world.delete(`${x},${y}`);
+				break sandFalling;
+			}
+			else if (world.get(`${x},${y}`) === undefined) {
+				world.set(`${x},${y}`, 'o');
+			}
+			else if (world.get(`${x},${y + 1}`) === undefined) {
+				world.delete(`${x},${y}`);
+				y++;
+				world.set(`${x},${y}`, 'o');
+			}
+			else if (world.get(`${x - 1},${y + 1}`) === undefined) {
+				world.delete(`${x},${y}`);
+				x--;
+				y++;
+				world.set(`${x},${y}`, 'o');
+			}
+			else if (world.get(`${x + 1},${y + 1}`) === undefined) {
+				world.delete(`${x},${y}`);
+				x++;
+				y++;
+				world.set(`${x},${y}`, 'o');
+			} else break;
+		}
+	}
+
+	return [...world.values()].filter((value: any) => value === 'o').length
+}
+
+function solveOne(data: string): any {
+	return simulateSand(...placeWalls(data, false));
 }
 
 
@@ -68,66 +79,13 @@ function solveOne(data: string): any {
 
 
 function solveTwo(data: string): any {
-	const world = new Map();
-	for (const coordList of data.split('\n')) {
-		let [x, y] = coordList.split(' -> ')[0].split(',').map(Number);
-		for (const coord of coordList.split(' -> ').slice(1)) {
-			const [x2, y2] = coord.split(',').map(Number);
-			while (x !== x2 || y !== y2) {
-				world.set(`${x},${y}`, (world.get(`${x},${y}`) || 0) + 1);
-				if (x !== x2) {
-					x += x < x2 ? 1 : -1;
-				}
-				if (y !== y2) {
-					y += y < y2 ? 1 : -1;
-				}
-			}
-			world.set(`${x},${y}`, (world.get(`${x},${y}`) || 0) + 1);
-		}
-	}
-	const greatestY = Math.max(...Array.from(world.keys()).map((key: string) => Number(key.split(',')[1])));
-
-	for (let x = -10000; x < 10000; x++) {
-		world.set(`${x},${greatestY + 2}`, 'f');
-	}
-	while (true) {
-		let sand = [500, 0]
-		if (world.get(`${sand[0]},${sand[1]}`) === 's') break;
-		while (true) {
-			if (world.get(`${sand[0]},${sand[1]}`) === undefined) {
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue
-			}
-			if (world.get(`${sand[0]},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			if (world.get(`${sand[0] - 1},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[0]--;
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			if (world.get(`${sand[0] + 1},${sand[1] + 1}`) === undefined) {
-				world.delete(`${sand[0]},${sand[1]}`);
-				sand[0]++;
-				sand[1]++;
-				world.set(`${sand[0]},${sand[1]}`, 's');
-				continue;
-			}
-			break;
-		}
-	}
-	return [...world.values()].filter((value: any) => value === 's').length;
+	return simulateSand(...placeWalls(data, true));
 }
 
 
 (() => {
 	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	/*assert.deepStrictEqual(solveTwo(`498,4 -> 498,6 -> 496,6
-503,4 -> 502,4 -> 502,9 -> 494,9`), 93);*/
-	console.log(solveTwo(data));
+	assert.deepStrictEqual(solveTwo(`498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9`), 93);
+	//console.log(solveTwo(data));
 })();
