35e51008050ca6e00622f82bfa9cd1cc2c9903f8
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-19 02:34:23 -0500

    Optimize 2022 Day 18 TypeScript solution

--- 2022/solutions/18/solve.ts
+++ 2022/solutions/18/solve.ts
@@ -1,32 +1,26 @@
 const fs = require('fs');
 const assert = require('assert');
 
-
-
-function solveOne(data: string): any {
-	const world = new Set<string>();
-	const lines = data.split('\n').map(l => l.split(',').map(Number)).forEach(([x, y, z]) => {
-		world.add(`${x},${y},${z}`);
-	})
-
-	let exposedFaces = 0;
-	// iterate through all nine directions from a single coordinate
-	for (const cube of world) {
-		const [x, y, z] = cube.split(',').map(Number);
-		for (let dx = -1; dx <= 1; dx++) {
-			for (let dy = -1; dy <= 1; dy++) {
-				for (let dz = -1; dz <= 1; dz++) {
-					const dist = Math.abs(dx) + Math.abs(dy) + Math.abs(dz);
-					if (dist !== 1) continue;
-					const neighbor = `${x + dx},${y + dy},${z + dz}`;
-					if (!world.has(neighbor)) {
-						exposedFaces++;
-					}
-				}
+function* generateTouchingCubes(x: number, y: number, z: number): Generator<[number, number, number]> {
+	for (let dx = -1; dx <= 1; dx++) {
+		for (let dy = -1; dy <= 1; dy++) {
+			for (let dz = -1; dz <= 1; dz++) {
+				const dist = Math.abs(dx) + Math.abs(dy) + Math.abs(dz);
+				if (dist === 1) yield [x + dx, y + dy, z + dz];
 			}
 		}
 	}
+}
 
+function solveOne(data: string): any {
+	const cubes = new Set<string>(data.split('\n'));
+
+	let exposedFaces = 0;
+	for (const cube of cubes) {
+		for (const [nx, ny, nz] of generateTouchingCubes(...cube.split(',').map(Number) as [number, number, number])) {
+			if (!cubes.has(`${nx},${ny},${nz}`)) exposedFaces++;
+		}
+	}
 	return exposedFaces;
 }
 
@@ -54,83 +48,42 @@ function solveOne(data: string): any {
 
 
 function solveTwo(data: string): any {
-	const world = new Map<string, 'C' | 'E' | 'I'>();
-	data.split('\n').map(l => l.split(',').map(Number)).forEach(([x, y, z]) => {
-		world.set(`${x},${y},${z}`, 'C');
-	})
-
-	// get min x, y, z, and max x, y, z
 	let minX = Infinity;
-	let minY = Infinity;
-	let minZ = Infinity;
 	let maxX = -Infinity;
+	let minY = Infinity;
 	let maxY = -Infinity;
+	let minZ = Infinity;
 	let maxZ = -Infinity;
-	for (const cube of world.keys()) {
+	const cubes = new Set<string>(data.split('\n').map(cube => {
 		const [x, y, z] = cube.split(',').map(Number);
 		minX = Math.min(minX, x);
-		minY = Math.min(minY, y);
-		minZ = Math.min(minZ, z);
 		maxX = Math.max(maxX, x);
+		minY = Math.min(minY, y);
 		maxY = Math.max(maxY, y);
+		minZ = Math.min(minZ, z);
 		maxZ = Math.max(maxZ, z);
-	}
+		return cube;
+	}));
 
-
-	function floodFill(world: Map<string, 'C' | 'E' | 'I'>, x: number, y: number, z: number, filling: 'E' | 'I'): void {
-		const queue = [[x, y, z]];
-		while (queue.length) {
-			const [x, y, z] = queue.shift()!;
-			const cube = `${x},${y},${z}`;
-			world.set(cube, filling);
-			for (let dx = -1; dx <= 1; dx++) {
-				for (let dy = -1; dy <= 1; dy++) {
-					for (let dz = -1; dz <= 1; dz++) {
-						const dist = Math.abs(dx) + Math.abs(dy) + Math.abs(dz);
-						if (dist !== 1) continue;
-						const nx = x + dx;
-						const ny = y + dy;
-						const nz = z + dz;
-						//if outside of the world, skip
-						if (nx < minX || nx > maxX || ny < minY || ny > maxY || nz < minZ || nz > maxZ) continue;
-						if (!world.has(`${nx},${ny},${nz}`)) queue.push([nx, ny, nz]);
-					}
-				}
-			}
-		}
-	}
+	const visited = new Set<string>();
 
 	let exposedFaces = 0;
-	function floodSearch(world: Map<string, 'C' | 'E' | 'I'>, x: number, y: number, z: number): void {
-		const stack = [[x, y, z]];
-		while (stack.length) {
-			const [x, y, z] = stack.pop()!;
-			const cube = `${x},${y},${z}`;
-			if (world.get(cube) === 'E') continue;
-			world.set(cube, 'E');
-			for (let dx = -1; dx <= 1; dx++) {
-				for (let dy = -1; dy <= 1; dy++) {
-					for (let dz = -1; dz <= 1; dz++) {
-						const dist = Math.abs(dx) + Math.abs(dy) + Math.abs(dz);
-						if (dist !== 1) continue;
-						const nx = x + dx;
-						const ny = y + dy;
-						const nz = z + dz;
-						//if outside of the world, skip
-						const key = `${nx},${ny},${nz}`;
+	const stack = [[minX - 1, minY - 1, minZ - 1]];
+	while (stack.length) {
+		const [x, y, z] = stack.pop()!;
+		const cube = `${x},${y},${z}`;
 
-						if (nx < minX - 1 || nx > maxX + 1 || ny < minY - 1 || ny > maxY + 1 || nz < minZ - 1 || nz > maxZ + 1) continue;
+		if (visited.has(cube)) continue;
+		visited.add(cube);
 
-						const neighborType = world.get(key);
-						if (neighborType === 'C') exposedFaces++
-						else if (!neighborType) stack.push([nx, ny, nz]);
-					}
-				}
-			}
+		for (const [nx, ny, nz] of generateTouchingCubes(x, y, z)) {
+			if (nx < minX - 1 || nx > maxX + 1 || ny < minY - 1 || ny > maxY + 1 || nz < minZ - 1 || nz > maxZ + 1) continue;
+
+			const key = `${nx},${ny},${nz}`;
+			if (cubes.has(key)) exposedFaces++
+			else if (!visited.has(key)) stack.push([nx, ny, nz]);
 		}
 	}
-
-	floodSearch(world, minX - 1, minY - 1, minZ - 1);
 
 	return exposedFaces;
 }
