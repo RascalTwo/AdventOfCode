b8aa6185f5bb758d0abb1cb09cfb83547db7a89a
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-23 01:32:58 -0500

    Optimize 2022 Day 23 TypeScript solution

--- 2022/solutions/23/solve.ts
+++ 2022/solutions/23/solve.ts
@@ -2,105 +2,82 @@ const fs = require('fs');
 const assert = require('assert');
 
 
-
-const rotatableArray = (arr: any[]) => {
-	return {
-		start: 0,
-		rotate() {
-			this.start++;
-			if (this.start >= arr.length) this.start = 0;
-		},
-		values() {
-			const result = [];
+const rotatableArray = <T>(arr: T[]) => ({
+	start: 0,
+	rotate() {
+		if (++this.start >= arr.length) this.start = 0;
+	},
+	[Symbol.iterator]() {
+		return (function* (this: { start: number }) {
 			for (let i = 0; i < arr.length; i++) {
-				result.push(arr[(this.start + i) % arr.length]);
+				yield arr[(this.start + i) % arr.length];
 			}
-			return result;
-		}
-	}
+		}).call(this);
+	},
+})
+
+const directionToOffset = {
+	north: [-1, 0],
+	south: [1, 0],
+	west: [0, -1],
+	east: [0, 1],
 }
 
-function solveOne(data: string): any {
-	const directions = rotatableArray(['north', 'south', 'west', 'east'])
+function solve(data: string, partTwo: boolean) {
+	const directions = rotatableArray<keyof typeof directionToOffset>(['north', 'south', 'west', 'east'])
 
 	const world = new Set<string>();
 	for (const [r, row] of data.split('\n').entries()) {
-		for (const [c, col] of row.split('').entries()) {
-			if (col === '#') {
-				world.add(`${r},${c}`);
-			}
+		for (const [c, col] of [...row].entries()) {
+			if (col === '#') world.add(`${r},${c}`);
 		}
 	}
 
-	const wantedMoves = new Map<string, string[]>();
-	for (let r = 0; r < 10; r++) {
-		wantedMoves.clear()
-		//visualize()
+	const moves = new Map<string, string[]>();
+	const rounds = partTwo ? Infinity : 10;
+	for (let r = 0; r < rounds; r++) {
 		for (const elf of world) {
 			const [er, ec] = elf.split(',').map(Number);
+
 			const neighbors = [];
 			for (let dr = -1; dr <= 1; dr++) {
 				for (let dc = -1; dc <= 1; dc++) {
-					if (dr === 0 && dc === 0) continue;
-					if (world.has(`${er + dr},${ec + dc}`)) {
+					if (!(dr === 0 && dc === 0) && world.has(`${er + dr},${ec + dc}`)) {
 						neighbors.push(`${er + dr},${ec + dc}`);
 					}
 				}
 			}
-
-
-
-			//visualize(er, ec)
-
 			if (neighbors.length === 0) continue;
-			const northNeighbors = neighbors.filter(n => +n.split(',')[0] < er);
-			const southNeighbors = neighbors.filter(n => +n.split(',')[0] > er);
-			const westNeighbors = neighbors.filter(n => +n.split(',')[1] < ec);
-			const eastNeighbors = neighbors.filter(n => +n.split(',')[1] > ec);
-			for (const direction of directions.values()) {
-				if (direction === 'north' && northNeighbors.length === 0) {
-					const northCoordinate = [er - 1, ec];
-					const desiringCoord = wantedMoves.get(northCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(northCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'south' && southNeighbors.length === 0) {
-					const southCoordinate = [er + 1, ec];
-					const desiringCoord = wantedMoves.get(southCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(southCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'west' && westNeighbors.length === 0) {
-					const westCoordinate = [er, ec - 1];
-					const desiringCoord = wantedMoves.get(westCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(westCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'east' && eastNeighbors.length === 0) {
-					const eastCoordinate = [er, ec + 1];
-					const desiringCoord = wantedMoves.get(eastCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(eastCoordinate.join(','), desiringCoord);
-					break;
-				}
+
+			const directionalNeighbors: Record<keyof typeof directionToOffset, (n: string) => boolean> = {
+				north: n => +n.split(',')[0] < er, south: n => +n.split(',')[0] > er,
+				west: n => +n.split(',')[1] < ec, east: n => +n.split(',')[1] > ec,
+			}
+			for (const direction of directions) {
+				if (neighbors.filter(directionalNeighbors[direction]).length > 0) continue;
+
+				const offset = directionToOffset[direction];
+				const coordinate = [er + offset[0], ec + offset[1]];
+				const otherElves = moves.get(coordinate.join(',')) || [];
+				otherElves.push(elf);
+				moves.set(coordinate.join(','), otherElves);
+				break;
 			}
 		}
 
-		for (const [coord, elves] of wantedMoves.entries()) {
-			if (elves.length > 1) continue;
-			const [r, c] = coord.split(',').map(Number);
-			const elf = elves[0];
+		let movesMade = !partTwo
+		for (const [coord, [elf, other]] of moves.entries()) {
+			if (other) continue;
 			world.delete(elf);
-			world.add(`${r},${c}`);
+			world.add(coord);
+			movesMade = true;
 		}
+		moves.clear()
+
+		if (!movesMade) return r + 1;
+
 		directions.rotate();
-
-		//visualize()
 	}
-
 
 	let minX = Infinity;
 	let maxX = -Infinity;
@@ -114,48 +91,19 @@ function solveOne(data: string): any {
 		maxY = Math.max(maxY, r);
 	}
 
-	function visualize(er?: number, ec?: number) {
-		let minX = Infinity;
-		let maxX = -Infinity;
-		let minY = Infinity;
-		let maxY = -Infinity;
-		for (const elf of world) {
-			const [r, c] = elf.split(',').map(Number);
-			minX = Math.min(minX, c);
-			maxX = Math.max(maxX, c);
-			minY = Math.min(minY, r);
-			maxY = Math.max(maxY, r);
-		}
-
-		let lines: string[] = ['---------------------']
-		for (let r = minY; r <= maxY; r++) {
-			let line = '';
-			for (let c = minX; c <= maxX; c++) {
-				if (r === er && c === ec) line += 'E';
-				else if (world.has(`${r},${c}`)) {
-					line += '#';
-				} else {
-					line += '.';
-				}
-			}
-			lines.push(line);
-		}
-		console.log(lines.join('\n'));
-	}
-
 	let result = 0;
 	for (let r = minY; r <= maxY; r++) {
 		for (let c = minX; c <= maxX; c++) {
-			if (!world.has(`${r},${c}`)) {
-				result++;
-			}
+			if (!world.has(`${r},${c}`)) result++;
 		}
 	}
 
-	visualize()
 	return result;
 }
 
+function solveOne(data: string): any {
+	return solve(data, false);
+}
 
 (() => {
 	const data = fs.readFileSync(__dirname + '/input.in').toString();
@@ -176,145 +124,8 @@ function solveOne(data: string): any {
 
 
 function solveTwo(data: string): any {
-	
-	const directions = rotatableArray(['north', 'south', 'west', 'east'])
-
-	const world = new Set<string>();
-	for (const [r, row] of data.split('\n').entries()) {
-		for (const [c, col] of row.split('').entries()) {
-			if (col === '#') {
-				world.add(`${r},${c}`);
-			}
-		}
-	}
-
-	const wantedMoves = new Map<string, string[]>();
-	for (let r = 0; r < 1_000_000; r++) {
-		wantedMoves.clear()
-		//visualize()
-		for (const elf of world) {
-			const [er, ec] = elf.split(',').map(Number);
-			const neighbors = [];
-			for (let dr = -1; dr <= 1; dr++) {
-				for (let dc = -1; dc <= 1; dc++) {
-					if (dr === 0 && dc === 0) continue;
-					if (world.has(`${er + dr},${ec + dc}`)) {
-						neighbors.push(`${er + dr},${ec + dc}`);
-					}
-				}
-			}
-
-
-
-			//visualize(er, ec)
-
-			if (neighbors.length === 0) continue;
-			const northNeighbors = neighbors.filter(n => +n.split(',')[0] < er);
-			const southNeighbors = neighbors.filter(n => +n.split(',')[0] > er);
-			const westNeighbors = neighbors.filter(n => +n.split(',')[1] < ec);
-			const eastNeighbors = neighbors.filter(n => +n.split(',')[1] > ec);
-			for (const direction of directions.values()) {
-				if (direction === 'north' && northNeighbors.length === 0) {
-					const northCoordinate = [er - 1, ec];
-					const desiringCoord = wantedMoves.get(northCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(northCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'south' && southNeighbors.length === 0) {
-					const southCoordinate = [er + 1, ec];
-					const desiringCoord = wantedMoves.get(southCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(southCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'west' && westNeighbors.length === 0) {
-					const westCoordinate = [er, ec - 1];
-					const desiringCoord = wantedMoves.get(westCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(westCoordinate.join(','), desiringCoord);
-					break;
-				}
-				if (direction === 'east' && eastNeighbors.length === 0) {
-					const eastCoordinate = [er, ec + 1];
-					const desiringCoord = wantedMoves.get(eastCoordinate.join(',')) || [];
-					desiringCoord.push(elf);
-					wantedMoves.set(eastCoordinate.join(','), desiringCoord);
-					break;
-				}
-			}
-		}
-
-		let movesMade = 0;
-		for (const [coord, elves] of wantedMoves.entries()) {
-			if (elves.length > 1) continue;
-			const [r, c] = coord.split(',').map(Number);
-			const elf = elves[0];
-			world.delete(elf);
-			world.add(`${r},${c}`);
-			movesMade++
-		}
-		if (movesMade === 0) return r + 1;
-		directions.rotate();
-
-		//visualize()
-	}
-
-
-	let minX = Infinity;
-	let maxX = -Infinity;
-	let minY = Infinity;
-	let maxY = -Infinity;
-	for (const elf of world) {
-		const [r, c] = elf.split(',').map(Number);
-		minX = Math.min(minX, c);
-		maxX = Math.max(maxX, c);
-		minY = Math.min(minY, r);
-		maxY = Math.max(maxY, r);
-	}
-
-	function visualize(er?: number, ec?: number) {
-		let minX = Infinity;
-		let maxX = -Infinity;
-		let minY = Infinity;
-		let maxY = -Infinity;
-		for (const elf of world) {
-			const [r, c] = elf.split(',').map(Number);
-			minX = Math.min(minX, c);
-			maxX = Math.max(maxX, c);
-			minY = Math.min(minY, r);
-			maxY = Math.max(maxY, r);
-		}
-
-		let lines: string[] = ['---------------------']
-		for (let r = minY; r <= maxY; r++) {
-			let line = '';
-			for (let c = minX; c <= maxX; c++) {
-				if (r === er && c === ec) line += 'E';
-				else if (world.has(`${r},${c}`)) {
-					line += '#';
-				} else {
-					line += '.';
-				}
-			}
-			lines.push(line);
-		}
-		console.log(lines.join('\n'));
-	}
-
-	let result = 0;
-	for (let r = minY; r <= maxY; r++) {
-		for (let c = minX; c <= maxX; c++) {
-			if (!world.has(`${r},${c}`)) {
-				result++;
-			}
-		}
-	}
-
-	visualize()
-	return result;
+	return solve(data, true);
 }
-
 
 (() => {
 	const data = fs.readFileSync(__dirname + '/input.in').toString();
