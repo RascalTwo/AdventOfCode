8198ef9cde98ac4a69312d936b19d201cf036752
Rascal_Two <therealrascaltwo@gmail.com>
2022-12-24 01:23:18 -0500

    Optimize 2022 Day 24 TypeScript solution

--- 2022/solutions/24/solve.ts
+++ 2022/solutions/24/solve.ts
@@ -2,85 +2,40 @@ const fs = require('fs');
 const assert = require('assert');
 
 
-interface Blizzard {
-	velocity: [number, number];
-}
-function solveOne(data: string): any {
+type World = Map<string, [number, number][] | '#'>;
+
+const parseWorld = (data: string): [number, number, World] => {
+	const charToValue: Record<string, any> = {
+		'#': '#',
+		'>': [[0, 1]],
+		'<': [[0, -1]],
+		'^': [[-1, 0]],
+		'v': [[1, 0]],
+	}
 	const rows = data.split('\n').length;
 	const columns = data.split('\n')[0].length;
 
-	const world = new Map<string, Blizzard[] | '#'>();
+	const world: World = new Map();
 	for (const [r, row] of data.split('\n').entries()) {
 		for (const [c, col] of [...row].entries()) {
-			if (col === '#') {
-				world.set(`${r},${c}`, '#');
-			}
-			else if (col === '>') {
-				world.set(`${r},${c}`, [{ velocity: [0, 1] }]);
-			}
-			else if (col === '<') {
-				world.set(`${r},${c}`, [{ velocity: [0, -1] }]);
-			}
-			else if (col === '^') {
-				world.set(`${r},${c}`, [{ velocity: [-1, 0] }]);
-			}
-			else if (col === 'v') {
-				world.set(`${r},${c}`, [{ velocity: [1, 0] }]);
-			}
+			const value = charToValue[col];
+			if (value) world.set(`${r},${c}`, value);
 		}
 	}
-	const end = [
-		data.split('\n').length - 1,
-		data.split('\n')[0].length - 2
-	]
 
-	world.set(`${end[0] + 1},${end[1]}`, '#');
+	world.set(`${rows},${columns - 2}`, '#');
 	world.set(`${-1},${1}`, '#');
 
-	function visualize() {
-		for (let r = 0; r < rows; r++) {
-			let row = '';
-			for (let c = 0; c < columns; c++) {
-				const blizzards = world.get(`${r},${c}`);
-				if (blizzards === '#') {
-					row += '#';
-				}
-				else if (blizzards) {
-					if (blizzards.length === 1) {
-						const [vr, vc] = blizzards[0].velocity;
-						if (vr === 1) {
-							row += 'v';
-						}
-						else if (vr === -1) {
-							row += '^';
-						}
-						else if (vc === 1) {
-							row += '>';
-						}
-						else if (vc === -1) {
-							row += '<';
-						}
-					}
-					else row += blizzards.length;
-				}
-				else {
-					row += '.';
-				}
-			}
-			console.log(row);
-		}
-	}
+	return [rows, columns, world];
+}
 
-	interface Game {
-		r: number;
-		c: number;
-	}
+const offsets = [[-1, 0], [0, 1], [1, 0], [0, -1], [0, 0]];
 
-	let elves = new Set<string>([`0,1`]);
-	let minutes = 0;
-	while (elves.size) {
-		minutes++;
-		const newWorld = new Map<string, Blizzard[] | '#'>();
+function minutesToNavigate(rows: number, columns: number, world: World, start: [number, number], [er, ec]: [number, number]) {
+	let elves = new Set<string>([start.join(',')]);
+
+	for (let m = 1; elves.size; m++) {
+		const newWorld: World = new Map();
 		for (const [key, blizzards] of world.entries()) {
 			if (blizzards === '#') {
 				newWorld.set(key, '#');
@@ -88,48 +43,44 @@ function solveOne(data: string): any {
 			}
 
 			const [r, c] = key.split(',').map(Number);
-			for (const blizzard of blizzards) {
-				const [vr, vc] = blizzard.velocity;
-				const next = [r + vr, c + vc];
-				if (world.get(`${next[0]},${next[1]}`) === '#') {
-					if (vr === 1) {
-						next[0] = 1
-					} else if (vr === -1) {
-						next[0] = rows - 2
-					} else if (vc === 1) {
-						next[1] = 1
-					} else if (vc === -1) {
-						next[1] = columns - 2
-					}
+			for (const velocity of blizzards) {
+				const [vr, vc] = velocity;
+				let [nr, nc] = [r + vr, c + vc];
+				if (world.get(`${nr},${nc}`) === '#') {
+					if (vr === 1) nr = 1
+					else if (vr === -1) nr = rows - 2
+					else if (vc === 1) nc = 1
+					else if (vc === -1) nc = columns - 2
 				}
-				const nextBlizzards = newWorld.get(`${next[0]},${next[1]}`) as Blizzard[];
-				if (nextBlizzards) nextBlizzards.push(blizzard);
-				else newWorld.set(`${next[0]},${next[1]}`, [blizzard]);
+
+				const nextBlizzards = newWorld.get(`${nr},${nc}`) as [number, number][];
+				if (nextBlizzards) nextBlizzards.push(velocity);
+				else newWorld.set(`${nr},${nc}`, [velocity]);
 			}
 		}
 
 		world.clear();
 		for (const [key, blizzards] of newWorld.entries()) world.set(key, blizzards);
+
 		const newElves = new Set<string>();
 		for (const elf of elves) {
-			const [r, c ] = elf.split(',').map(Number);
-			for (const dr of [-1, 0, 1]) {
-				for (const dc of [-1, 0, 1]) {
-					const dist = Math.abs(dr) + Math.abs(dc);
-					if (dist > 1) continue;
-					const next = [r + dr, c + dc];
-					if (world.has(`${next[0]},${next[1]}`)) continue;
-					if (next[0] === end[0] && next[1] === end[1]) {
-						return minutes
-					}
-					newElves.add(`${next[0]},${next[1]}`);
-				}
+			const [r, c] = elf.split(',').map(Number);
+			for (const [dr, dc] of offsets) {
+				const [nr, nc] = [r + dr, c + dc];
+				if (nr === er && nc === ec) return m;
+
+				if (!world.has(`${nr},${nc}`)) newElves.add(`${nr},${nc}`);
 			}
 		}
 		elves = newElves;
 	}
 
 	return -1;
+}
+
+function solveOne(data: string): any {
+	const [rows, columns, world] = parseWorld(data);
+	return minutesToNavigate(rows, columns, world, [0, 1], [rows - 1, columns - 2]);
 }
 
 
@@ -146,238 +97,12 @@ function solveOne(data: string): any {
 
 
 function solveTwo(data: string): any {
-	const rows = data.split('\n').length;
-	const columns = data.split('\n')[0].length;
-
-	const world = new Map<string, Blizzard[] | '#'>();
-	for (const [r, row] of data.split('\n').entries()) {
-		for (const [c, col] of [...row].entries()) {
-			if (col === '#') {
-				world.set(`${r},${c}`, '#');
-			}
-			else if (col === '>') {
-				world.set(`${r},${c}`, [{ velocity: [0, 1] }]);
-			}
-			else if (col === '<') {
-				world.set(`${r},${c}`, [{ velocity: [0, -1] }]);
-			}
-			else if (col === '^') {
-				world.set(`${r},${c}`, [{ velocity: [-1, 0] }]);
-			}
-			else if (col === 'v') {
-				world.set(`${r},${c}`, [{ velocity: [1, 0] }]);
-			}
-		}
-	}
-	let end = [
-		data.split('\n').length - 1,
-		data.split('\n')[0].length - 2
-	]
-
-	world.set(`${end[0] + 1},${end[1]}`, '#');
-	world.set(`${-1},${1}`, '#');
-
-	function visualize() {
-		for (let r = 0; r < rows; r++) {
-			let row = '';
-			for (let c = 0; c < columns; c++) {
-				const blizzards = world.get(`${r},${c}`);
-				if (blizzards === '#') {
-					row += '#';
-				}
-				else if (blizzards) {
-					if (blizzards.length === 1) {
-						const [vr, vc] = blizzards[0].velocity;
-						if (vr === 1) {
-							row += 'v';
-						}
-						else if (vr === -1) {
-							row += '^';
-						}
-						else if (vc === 1) {
-							row += '>';
-						}
-						else if (vc === -1) {
-							row += '<';
-						}
-					}
-					else row += blizzards.length;
-				}
-				else {
-					row += '.';
-				}
-			}
-			console.log(row);
-		}
-	}
-
-	interface Game {
-		r: number;
-		c: number;
-	}
-
-	let elves = new Set<string>([`0,1`]);
-	let minutes = 0;
-	first: while (elves.size) {
-		minutes++;
-		const newWorld = new Map<string, Blizzard[] | '#'>();
-		for (const [key, blizzards] of world.entries()) {
-			if (blizzards === '#') {
-				newWorld.set(key, '#');
-				continue;
-			}
-
-			const [r, c] = key.split(',').map(Number);
-			for (const blizzard of blizzards) {
-				const [vr, vc] = blizzard.velocity;
-				const next = [r + vr, c + vc];
-				if (world.get(`${next[0]},${next[1]}`) === '#') {
-					if (vr === 1) {
-						next[0] = 1
-					} else if (vr === -1) {
-						next[0] = rows - 2
-					} else if (vc === 1) {
-						next[1] = 1
-					} else if (vc === -1) {
-						next[1] = columns - 2
-					}
-				}
-				const nextBlizzards = newWorld.get(`${next[0]},${next[1]}`) as Blizzard[];
-				if (nextBlizzards) nextBlizzards.push(blizzard);
-				else newWorld.set(`${next[0]},${next[1]}`, [blizzard]);
-			}
-		}
-
-		world.clear();
-		for (const [key, blizzards] of newWorld.entries()) world.set(key, blizzards);
-		const newElves = new Set<string>();
-		for (const elf of elves) {
-			const [r, c ] = elf.split(',').map(Number);
-			for (const dr of [-1, 0, 1]) {
-				for (const dc of [-1, 0, 1]) {
-					const dist = Math.abs(dr) + Math.abs(dc);
-					if (dist > 1) continue;
-					const next = [r + dr, c + dc];
-					if (world.has(`${next[0]},${next[1]}`)) continue;
-					if (next[0] === end[0] && next[1] === end[1]) {
-						elves = new Set<string>([`${next[0]},${next[1]}`]);
-						break first;
-					}
-					newElves.add(`${next[0]},${next[1]}`);
-				}
-			}
-		}
-		elves = newElves;
-	}
-
-	end = [0, 1]
-	second: while (elves.size) {
-		minutes++;
-		const newWorld = new Map<string, Blizzard[] | '#'>();
-		for (const [key, blizzards] of world.entries()) {
-			if (blizzards === '#') {
-				newWorld.set(key, '#');
-				continue;
-			}
-
-			const [r, c] = key.split(',').map(Number);
-			for (const blizzard of blizzards) {
-				const [vr, vc] = blizzard.velocity;
-				const next = [r + vr, c + vc];
-				if (world.get(`${next[0]},${next[1]}`) === '#') {
-					if (vr === 1) {
-						next[0] = 1
-					} else if (vr === -1) {
-						next[0] = rows - 2
-					} else if (vc === 1) {
-						next[1] = 1
-					} else if (vc === -1) {
-						next[1] = columns - 2
-					}
-				}
-				const nextBlizzards = newWorld.get(`${next[0]},${next[1]}`) as Blizzard[];
-				if (nextBlizzards) nextBlizzards.push(blizzard);
-				else newWorld.set(`${next[0]},${next[1]}`, [blizzard]);
-			}
-		}
-
-		world.clear();
-		for (const [key, blizzards] of newWorld.entries()) world.set(key, blizzards);
-		const newElves = new Set<string>();
-		for (const elf of elves) {
-			const [r, c ] = elf.split(',').map(Number);
-			for (const dr of [-1, 0, 1]) {
-				for (const dc of [-1, 0, 1]) {
-					const dist = Math.abs(dr) + Math.abs(dc);
-					if (dist > 1) continue;
-					const next = [r + dr, c + dc];
-					if (world.has(`${next[0]},${next[1]}`)) continue;
-					if (next[0] === end[0] && next[1] === end[1]) {
-						elves = new Set<string>([`${next[0]},${next[1]}`]);
-						break second;
-					}
-					newElves.add(`${next[0]},${next[1]}`);
-				}
-			}
-		}
-		elves = newElves;
-	}
-	end = [
-		data.split('\n').length - 1,
-		data.split('\n')[0].length - 2
-	]
-	while (elves.size) {
-		minutes++;
-		const newWorld = new Map<string, Blizzard[] | '#'>();
-		for (const [key, blizzards] of world.entries()) {
-			if (blizzards === '#') {
-				newWorld.set(key, '#');
-				continue;
-			}
-
-			const [r, c] = key.split(',').map(Number);
-			for (const blizzard of blizzards) {
-				const [vr, vc] = blizzard.velocity;
-				const next = [r + vr, c + vc];
-				if (world.get(`${next[0]},${next[1]}`) === '#') {
-					if (vr === 1) {
-						next[0] = 1
-					} else if (vr === -1) {
-						next[0] = rows - 2
-					} else if (vc === 1) {
-						next[1] = 1
-					} else if (vc === -1) {
-						next[1] = columns - 2
-					}
-				}
-				const nextBlizzards = newWorld.get(`${next[0]},${next[1]}`) as Blizzard[];
-				if (nextBlizzards) nextBlizzards.push(blizzard);
-				else newWorld.set(`${next[0]},${next[1]}`, [blizzard]);
-			}
-		}
-
-		world.clear();
-		for (const [key, blizzards] of newWorld.entries()) world.set(key, blizzards);
-		const newElves = new Set<string>();
-		for (const elf of elves) {
-			const [r, c ] = elf.split(',').map(Number);
-			for (const dr of [-1, 0, 1]) {
-				for (const dc of [-1, 0, 1]) {
-					const dist = Math.abs(dr) + Math.abs(dc);
-					if (dist > 1) continue;
-					const next = [r + dr, c + dc];
-					if (world.has(`${next[0]},${next[1]}`)) continue;
-					if (next[0] === end[0] && next[1] === end[1]) {
-						return minutes;
-					}
-					newElves.add(`${next[0]},${next[1]}`);
-				}
-			}
-		}
-		elves = newElves;
-	}
-
-	return -1;
+	const [rows, columns, world] = parseWorld(data);
+	return (
+			minutesToNavigate(rows, columns, world, [0, 1], [rows - 1, columns - 2])
+		+ minutesToNavigate(rows, columns, world, [rows - 1, columns - 2], [0, 1])
+		+ minutesToNavigate(rows, columns, world, [0, 1], [rows - 1, columns - 2])
+	);
 }
 
 
