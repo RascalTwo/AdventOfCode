8f328e2d297bb4ca2e718dcba2a95972d57e6a44
Rascal_Two <therealrascaltwo@gmail.com>
2023-12-10 21:34:30 -0600

    Optimize 2023 Day 10 TypeScript solution

--- 2023/10/solve.ts
+++ 2023/10/solve.ts
@@ -17,22 +17,17 @@ const CHAR_TO_DIRECTIONS = {
 	'|': [Direction.Up, Direction.Down],
 	'J': [Direction.Up, Direction.Left],
 	'-': [Direction.Left, Direction.Right],
-	'L': [Direction.Up, Direction.Right]
+	'L': [Direction.Up, Direction.Right],
+	'.': [],
 }
+
+const DIRECTIONS_TO_CHARS = Object.fromEntries(Object.entries(CHAR_TO_DIRECTIONS).map(([char, dirs]) => [JSON.stringify(dirs.sort()), char]))
 
 const DIRECTIONS_TO_OFFSETS = {
 	[Direction.Up]: [-1, 0],
 	[Direction.Right]: [0, 1],
 	[Direction.Down]: [1, 0],
 	[Direction.Left]: [0, -1]
-}
-
-function directionsToChar(directions: [Direction, Direction]): keyof typeof CHAR_TO_DIRECTIONS {
-	for (const [char, dirs] of Object.entries(CHAR_TO_DIRECTIONS)) {
-		if (JSON.stringify(dirs.sort()) === JSON.stringify(directions.sort())) return char as keyof typeof CHAR_TO_DIRECTIONS;
-	}
-
-	throw new Error('No match found')
 }
 
 type Loc = {
@@ -74,27 +69,31 @@ function calculateStartChar(field: Loc[][], start: [number, number]) {
 	if (field[start[0] + 1]?.[start[1]].directions.includes(Direction.Up))
 		field[start[0]][start[1]].directions.push(Direction.Down)
 
-	field[start[0]][start[1]].char = directionsToChar(field[start[0]][start[1]].directions as [Direction, Direction])
+	field[start[0]][start[1]].char = DIRECTIONS_TO_CHARS[JSON.stringify(field[start[0]][start[1]].directions.sort())]
+}
+
+function calculateMainLoopDistances(field: Loc[][], start: [number, number]) {
+	{
+		const queue = [{ loc: start, distance: 0 }]
+		while (queue.length) {
+			const { loc, distance } = queue.pop()!;
+			if (field[loc[0]][loc[1]].distance !== Number.MIN_SAFE_INTEGER && field[loc[0]][loc[1]].distance <= distance) continue;
+			field[loc[0]][loc[1]].distance = distance;
+			for (const direction of field[loc[0]][loc[1]].directions) {
+				const [ro, co] = DIRECTIONS_TO_OFFSETS[direction];
+				queue.push({ loc: [loc[0] + ro, loc[1] + co], distance: distance + 1 })
+			}
+			queue.sort((a, b) => b.distance - a.distance)
+		}
+	}
 }
 
 function solveOne(data: string): any {
 	const [start, field] = parseField(data);
 	calculateStartChar(field, start);
+	calculateMainLoopDistances(field, start);
 
-	const stack = [{ loc: start, distance: 0 }]
-
-	while (stack.length) {
-		const { loc, distance } = stack.shift()!;
-		if (field[loc[0]][loc[1]].distance !== Number.MIN_SAFE_INTEGER && field[loc[0]][loc[1]].distance <= distance) continue;
-		field[loc[0]][loc[1]].distance = distance;
-		for (const direction of field[loc[0]][loc[1]].directions) {
-			const [dr, dc] = DIRECTIONS_TO_OFFSETS[direction];
-			stack.push({ loc: [loc[0] + dr, loc[1] + dc], distance: distance + 1 })
-		}
-		stack.sort((a, b) => a.distance - b.distance)
-	}
-
-	return Math.max(...field.flat().map(p => p.distance === Number.MIN_SAFE_INTEGER ? 0 : p.distance));
+	return Math.max(...field.flat().map(({ distance }) => distance === Number.MIN_SAFE_INTEGER ? 0 : distance));
 }
 
 
@@ -113,86 +112,81 @@ LJ...`), 8);
 	console.log(solveOne(data));
 })();
 
+function expandMatrix<T>(matrix: T[][], char: T) {
+	for (let r = 0; r < matrix.length; r++)
+		matrix[r] = [char, ...matrix[r], char];
+	matrix.unshift(Array(matrix[0].length).fill(char));
+	matrix.push(Array(matrix[0].length).fill(char));
+	return matrix
+}
+
+function discardExtraPipes(field: Loc[][]) {
+	for (const row of field)
+		for (const loc of row)
+			if (loc.distance === Number.MIN_SAFE_INTEGER)
+				Object.assign(loc, { char: '.', directions: [] })
+}
+
+const CHARS_TO_PIXELS = Object.fromEntries(Object.entries(CHAR_TO_DIRECTIONS).map(([char, dirs]) => {
+	const pixels = [[0, 0, 0], [0, +!!dirs.length, 0], [0, 0, 0]]
+	for (const direction of dirs) {
+		const [ro, co] = DIRECTIONS_TO_OFFSETS[direction];
+		pixels[1 + ro][1 + co] = 1;
+	}
+	return [char, pixels]
+}))
+
+function zoomOut(field: Loc[][]) {
+	const walls = [];
+	for (const row of field) {
+		const rows: number[][] = [[], [], []]
+		for (const loc of row)
+			for (const [l, line] of CHARS_TO_PIXELS[loc.char].entries())
+				rows[l].push(...line);
+		walls.push(...rows);
+	}
+	return walls
+}
+
+function floodFill(walls: number[][]) {
+	const stack = [[0, 0]]
+	while (stack.length) {
+		const [r, c] = stack.shift()!;
+		if (walls[r][c] !== 0) continue;
+
+		walls[r][c] = 2;
+		for (const [ro, co] of Object.values(DIRECTIONS_TO_OFFSETS)) {
+			const nr = r + ro;
+			const nc = c + co;
+			if (walls[nr]?.[nc] !== undefined) stack.push([nr, nc])
+		}
+	}
+
+	return walls
+}
+
+function countDrySpaces(walls: number[][]) {
+	let count = 0
+	for (let r = 4; r < walls.length - 4; r += 3)
+		for (let c = 4; c < walls[r].length - 4; c += 3)
+			count += +(walls[r][c] === 0)
+
+	return count
+}
 
 function solveTwo(data: string): any {
-	const lines = data.trim().split('\n').map(line => line.split(''));
-	for (let r = 0; r < lines.length; r++)
-		lines[r] = ['.', ...lines[r], '.'];
-	lines.unshift(Array(lines[0].length).fill('.'));
-	lines.push(Array(lines[0].length).fill('.'));
-	data = lines.map(line => line.join('')).join('\n');
+	data = expandMatrix(data.trim().split('\n').map(line => line.split('')), '.').map(line => line.join('')).join('\n');
 
 	const [start, field] = parseField(data);
 	calculateStartChar(field, start);
+	calculateMainLoopDistances(field, start);
 
-	const loopStack = [{ loc: start, distance: 0 }]
+	discardExtraPipes(field);
 
-	while (loopStack.length) {
-		const { loc, distance } = loopStack.shift()!;
-		if (field[loc[0]][loc[1]].distance !== Number.MIN_SAFE_INTEGER && field[loc[0]][loc[1]].distance <= distance) continue;
-		field[loc[0]][loc[1]].distance = distance;
-		for (const direction of field[loc[0]][loc[1]].directions) {
-			const [dr, dc] = DIRECTIONS_TO_OFFSETS[direction];
-			loopStack.push({ loc: [loc[0] + dr, loc[1] + dc], distance: distance + 1 })
-		}
-		loopStack.sort((a, b) => a.distance - b.distance)
-	}
+	const walls = zoomOut(field);
+	floodFill(walls);
 
-	for (const row of field) {
-		for (const loc of row) {
-			if (loc.distance === Number.MIN_SAFE_INTEGER) {
-				loc.char = '.'
-				loc.directions = []
-			}
-		}
-	}
-
-	const walls = [];
-	for (const [r, row] of field.entries()) {
-		const rows: number[][] = [[], [], []]
-		for (const [c, loc] of row.entries()) {
-			const subCell = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-			subCell[1][1] = loc.char === '.' ? 0 : 1;
-			for (const direction of loc.directions) {
-				const [dr, dc] = DIRECTIONS_TO_OFFSETS[direction];
-				subCell[1 + dr][1 + dc] = 1;
-			}
-			for (const [l, line] of subCell.entries()) {
-				rows[l].push(...line);
-			}
-		}
-		walls.push(...rows);
-	}
-
-	const stack = [[0, 0]]
-	const visited = new Set<string>();
-	while (stack.length) {
-		const [r, c] = stack.shift()!;
-		const key = r + ',' + c;
-
-		if (visited.has(key)) continue;
-		visited.add(key);
-
-		if (walls[r][c] === 1) continue;
-
-		walls[r][c] = 2;
-		for (const [dr, dc] of [[-1, 0], [0, 1], [1, 0], [0, -1]]) {
-			const nr = r + dr;
-			const nc = c + dc;
-			if (walls[nr]?.[nc] === undefined) continue
-			stack.push([nr, nc])
-		}
-	}
-
-	let count = 0
-	for (let r = 1 + 3; r < walls.length - 1 - 3; r += 3) {
-		for (let c = 1 + 3; c < walls[r].length - 1 - 3; c += 3) {
-			const char = walls[r][c];
-			if (char === 0) count++
-			walls[r][c] = 3;
-		}
-	}
-	return count;
+	return countDrySpaces(walls);
 }
 
 
