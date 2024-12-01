0fc11fb017d5cf759a7df18f94f08a20278a085b
Rascal Two <therealrascaltwo@gmail.com>
2024-12-01 01:06:35 -0500

    Optimize 2024 Day 01 TypeScript solution

--- 2024/01/solve.ts
+++ 2024/01/solve.ts
@@ -2,18 +2,28 @@ const fs = require('fs');
 const assert = require('assert');
 
 
+function parseLists(data: string): [number[], number[]] {
+	const lists: [number[], number[]] = [[], []]
+	for (const line of data.split('\n')) {
+		for (const [i, digits] of line.trim().split(/\s+/).entries()) {
+			lists[i].push(+digits)
+		}
+	}
+	return lists
+}
 
 function solveOne(data: string): any {
-	data = data.trim()
-	const lists = [[], []]
-	data.split('\n').map((line, i) => {
-		line.trim().split(/\s+/).map((p, i) => lists[i].push(+p))
-	})
-	lists.forEach(l => l.sort((a, b) => a - b))
-	const distances = lists[0].map((_, i) => {
-		return Math.abs(lists[0][i] - lists[1][i])
-	})
-	return distances.reduce((a, b) => a + b, 0)
+	const [first, second] = parseLists(data)
+
+	first.sort((a, b) => a - b)
+	second.sort((a, b) => a - b)
+
+	let totalDistance = 0
+	for (let i = 0; i < first.length; i++) {
+		totalDistance += Math.abs(first[i] - second[i])
+	}
+
+	return totalDistance
 }
 
 
@@ -25,26 +35,23 @@ function solveOne(data: string): any {
 1   3
 3   9
 3   3`), 11);
-	console.log(solveOne(data));
+	assert.deepStrictEqual(solveOne(data), 2375403)
 })();
 
 
 function solveTwo(data: string): any {
-	data = data.trim()
-	const lists = [[], []]
-	data.split('\n').map((line, i) => {
-		line.trim().split(/\s+/).map((p, i) => lists[i].push(+p))
-	})
-	const inList2 = {};
-	for (const char of lists[1]) {
-		if (!(char in inList2)) inList2[char] = 0
-		inList2[char] += 1
+	const [first, second] = parseLists(data)
+
+	const listTwoCounts: Record<string, number> = {};
+	for (const num of second) {
+		listTwoCounts[num] = (listTwoCounts[num] ?? 0) + 1
 	}
 
-	const scores = lists[0].map(c => {
-		return c * (inList2[c] ?? 0)
-	})
-	return scores.reduce((a, b) => a + b, 0)
+	let similarityScore = 0;
+	for (const num of first) {
+		similarityScore += num * (listTwoCounts[num] ?? 0);
+	}
+	return similarityScore;
 }
 
 
@@ -57,5 +64,5 @@ function solveTwo(data: string): any {
 3   9
 3   3
 `), 31);
-	console.log(solveTwo(data));
+	assert.deepStrictEqual(solveTwo(data), 23082277)
 })();
