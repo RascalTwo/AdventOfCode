348adccfb25716d657cfd3d369086f95ca9b32c9
Rascal Two <therealrascaltwo@gmail.com>
2024-12-02 08:31:59 -0500

    Optimize 2024 Day 02 Typescript Solution

--- 2024/02/solve.ts
+++ 2024/02/solve.ts
@@ -2,90 +2,56 @@ const fs = require('fs');
 const assert = require('assert');
 
 
+function getDifferenceDirection(a: number, b: number) {
+	return a > b ? 'desc' : 'asc'
+}
+
+function findUnsafeLevel(levels: number[]) {
+	const dir = getDifferenceDirection(levels[0], levels[1])
+	for (let i = 0; i < levels.length - 1; i++) {
+		const current = levels[i];
+		const next = levels[i + 1];
+
+		const diff = Math.abs(current - next)
+		if (!(1 <= diff && diff <= 3)) {
+			return i
+		}
+
+		if (dir !== getDifferenceDirection(current, next)) {
+			return i
+		}
+	}
+
+	return -1;
+}
+
+function isReportSafe(levels: number[]) {
+	return findUnsafeLevel(levels) === -1
+}
+
+function couldReportBeSafe(levels: number[]) {
+	const unsafeLevel = findUnsafeLevel(levels)
+	const start = Math.max(0, unsafeLevel - 1)
+	const end = Math.min(levels.length, unsafeLevel + 2)
+
+	for (let levelToRemove = start; levelToRemove < end; levelToRemove++) {
+		const newLevels = levels.filter((_, i) => i !== levelToRemove)
+		if (isReportSafe(newLevels)) return true;
+	}
+	return false
+}
+
+
+function solve(data: string, isSafe: (levels: number[]) => boolean): any {
+	let totalSafe = 0;
+	for (const line of data.split('\n')) {
+		if (isSafe(line.split(' ').map(Number))) totalSafe++
+	}
+	return totalSafe
+}
 
 function solveOne(data: string): any {
-	data = data.trim()
-	let safeCount = 0;
-	for (const line of data.split('\n')) {
-		const nums = line.split(' ').map(Number);
-		let safe = true
-		let dir = null
-		for (let i = 0; i < nums.length - 1; i++) {
-			if (Math.abs(nums[i] - nums[i + 1]) > 3 || nums[i] - nums[i + 1] === 0) {
-				safe = false
-				break
-			}
-			if (!dir) dir = nums[i] > nums[i + 1] ? 'desc' : 'asc'
-			else {
-				if (dir === 'asc' && nums[i] > nums[i + 1]) {
-					safe = false
-					break
-				}
-				else if (dir === 'desc' && nums[i] < nums[i + 1]) {
-					safe = false;
-					break
-				}
-			}
-		}
-		if (safe) safeCount++
-	}
-	return safeCount
-}
-
-function wouldBeSafeByRemovingLevel(nums: number[]) {
-	for (let l = 0; l < nums.length; l++) {
-		const newNums = [...nums]
-		newNums.splice(l, 1)
-		let safe = true
-		let dir = null
-		for (let i = 0; i < newNums.length - 1; i++) {
-			if (Math.abs(newNums[i] - newNums[i + 1]) > 3 || newNums[i] - newNums[i + 1] === 0) {
-				safe = false
-				break
-			}
-			if (!dir) dir = newNums[i] > newNums[i + 1] ? 'desc' : 'asc'
-			else {
-				if (dir === 'asc' && newNums[i] > newNums[i + 1]) {
-					safe = false
-					break
-				}
-				else if (dir === 'desc' && newNums[i] < newNums[i + 1]) {
-					safe = false;
-					break
-				}
-			}
-		}
-		if (safe) return true;
-	}
-}
-
-function solveTwo(data: string): any {
-	data = data.trim()
-	let safeCount = 0;
-	for (const line of data.split('\n')) {
-		const nums = line.split(' ').map(Number);
-		let safe = true
-		let dir = null
-		for (let i = 0; i < nums.length - 1; i++) {
-			if (Math.abs(nums[i] - nums[i + 1]) > 3 || nums[i] - nums[i + 1] === 0) {
-				safe = false
-				break
-			}
-			if (!dir) dir = nums[i] > nums[i + 1] ? 'desc' : 'asc'
-			else {
-				if (dir === 'asc' && nums[i] > nums[i + 1]) {
-					safe = false
-					break
-				}
-				else if (dir === 'desc' && nums[i] < nums[i + 1]) {
-					safe = false;
-					break
-				}
-			}
-		}
-		if (safe || wouldBeSafeByRemovingLevel(nums)) safeCount++
-	}
-	return safeCount
+	return solve(data, isReportSafe)
 }
 
 (() => {
@@ -96,10 +62,13 @@ function solveTwo(data: string): any {
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9`), 2);
-	console.log(solveOne(data));
+	assert.deepStrictEqual(solveOne(data), 463);
 })();
 
 
+function solveTwo(data: string): any {
+	return solve(data, levels => isReportSafe(levels) || couldReportBeSafe(levels))
+}
 
 (() => {
 	const data = fs.readFileSync(__dirname + '/input.in').toString();
@@ -109,5 +78,5 @@ function solveTwo(data: string): any {
 1 3 2 4 5
 8 6 4 4 1
 1 3 6 7 9`), 4);
-	console.log(solveTwo(data));
+	assert.deepStrictEqual(solveTwo(data), 514);
 })();
