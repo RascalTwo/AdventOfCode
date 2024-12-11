2da1e526ff4699cdf0e8611aac2c5559dd106a6d
Rascal Two <therealrascaltwo@gmail.com>
2024-12-11 15:45:45 -0500

    Optimize 2024 Day 06 Typescript Solution

--- 2024/06/solve.ts
+++ 2024/06/solve.ts
@@ -1,42 +1,71 @@
 const fs = require('fs');
 const assert = require('assert');
 
-const charToDir = (char: string) => {
-	if (char === '^') return [-1, 0]
-	else if (char === '>') return [0, 1]
-	else if (char === 'V') return [1, 0]
-	else if (char === '<') return [0, -1]
-	throw new Error('Bad char: ' + char)
-}
+type Loc2 = { r: number, c: number, direction: [number, number] }
 
-const chars = '^>V<'
+const GUARD_DIRECTIONS: [number, number][] = [
+	[-1, 0],
+	[0, 1],
+	[1, 0],
+	[0, -1],
+]
 
-function solveOne(data: string): any {
-	const world = data.split('\n');
-	let loc = { r: 0, c: 0, char: '^' }
+function parseMap(data: string) {
+	const world = data.split('\n').map(l => [...l]);
 	for (let r = 0; r < world.length; r++) {
 		for (let c = 0; c < world[r].length; c++) {
 			if (world[r][c] === '^') {
-				loc = { r, c, char: '^' }
-				break
+				return {
+					world,
+					loc: { r, c, direction: GUARD_DIRECTIONS[0] } as Loc2
+				}
 			}
 		}
 	}
+}
 
-	let visited = []
+function pathfindRobot(loc: Loc2, world: string[][]) {
+	let visited = new Set<string>();
 	while (loc.r >= 0 && loc.r < world.length && loc.c >= 0 && loc.c < world[0].length) {
-		visited.push({ ...loc })
-		const [ro, co] = charToDir(loc.char)
+		const locJSON = JSON.stringify(loc)
+		if (visited.has(locJSON)) {
+			return {
+				loop: true,
+				visited
+			}
+		}
+		visited.add(locJSON)
+
+		const [ro, co] = loc.direction
 		const [nr, nc] = [loc.r + ro, loc.c + co]
 		const inFront = world[nr]?.[nc]
 		if (inFront === '#') {
-			loc.char = chars[(chars.indexOf(loc.char) + 1) % chars.length]
+			loc.direction = GUARD_DIRECTIONS[(GUARD_DIRECTIONS.indexOf(loc.direction) + 1) % GUARD_DIRECTIONS.length]
 		} else {
 			loc.r = nr;
 			loc.c = nc;
 		}
 	}
-	return new Set(visited.map(v => ({ r: v.r, c: v.c })).map(v => JSON.stringify(v))).size
+	return {
+		loop: false,
+		visited
+	};
+}
+
+function getRidOfDuplicateLocations(visited: Set<string>) {
+	return new Set(
+		[...visited]
+			.map(json => JSON.parse(json) as Loc2)
+			.map(({ r, c }) => ({ r, c }))
+			.map(loc => JSON.stringify(loc))
+	)
+}
+
+function solveOne(data: string): any {
+	const { world, loc } = parseMap(data)!
+
+	const { visited } = pathfindRobot(loc, world)
+	return getRidOfDuplicateLocations(visited).size
 }
 
 
@@ -55,138 +84,19 @@ function solveOne(data: string): any {
 	console.log(solveOne(data));
 })();
 
-function navTheWorldEndsInLoop(loc: { r: number, c: number, char: string }, world: string[][]) {
-	let visited = new Set<string>();
-	while (loc.r >= 0 && loc.r < world.length && loc.c >= 0 && loc.c < world[0].length) {
-		const locJSON = JSON.stringify(loc)
-		if (visited.has(locJSON)) {
-			return true
-		}
-		visited.add(locJSON)
-		const [ro, co] = charToDir(loc.char)
-		const [nr, nc] = [loc.r + ro, loc.c + co]
-		const inFront = world[nr]?.[nc]
-		if (inFront === '#') {
-			loc.char = chars[(chars.indexOf(loc.char) + 1) % chars.length]
-		} else {
-			loc.r = nr;
-			loc.c = nc;
-		}
-	}
-	return false;
-}
-
-type Loc2 = { r: number, c: number, char: string }
-
-function isBetween(small: number, large: number, needle: number) {
-	if (small > large) {
-		[small, large] = [large, small]
-	}
-	return small <= needle && needle <= large
-}
 
 function solveTwo(data: string): any {
-	const world = data.split('\n').map(l => [...l]);
-	let loc = { r: 0, c: 0, char: '^' }
-	for (let r = 0; r < world.length; r++) {
-		for (let c = 0; c < world[r].length; c++) {
-			if (world[r][c] === '^') {
-				loc = { r, c, char: '^' }
-				break
-			}
-		}
-	}
-	const startingLoc = { ...loc }
+	const { loc, world } = parseMap(data)!;
+	const { visited } = pathfindRobot({ ...loc }, world)
+	const possibleObstructions = [...getRidOfDuplicateLocations(visited)].map(s => JSON.parse(s))
 
-	/*
-	const pathingLines: { from: { r: number, c: number }, to: { r: number, c: number }, char: string }[] = []
-
-	function moveLocUntilEnd(loc: Loc2) {
-		while (loc.r >= 0 && loc.r < world.length && loc.c >= 0 && loc.c < world[0].length) {
-			const [ro, co] = charToDir(loc.char)
-			const [nr, nc] = [loc.r + ro, loc.c + co]
-			const inFront = world[nr]?.[nc]
-			if (inFront === '#' || inFront === undefined) return loc
-			loc.r = nr
-			loc.c = nc
-		}
-		return loc
-	}*/
-
-
-	/*
-	function addPathingLine() {
-		const startLoc = moveLocUntilEnd({ ...loc, char: chars[(chars.indexOf(loc.char) + 2) % chars.length] })
-		const endLoc = moveLocUntilEnd({ ...loc })
-		pathingLines.push({ from: { r: startLoc.r, c: startLoc.c }, to: { r: endLoc.r, c: endLoc.c }, char: endLoc.char })
-	}
-
-	addPathingLine()*/
-
-	let visited = []
-	/*
-	while (loc.r >= 0 && loc.r < world.length && loc.c >= 0 && loc.c < world[0].length) {
-		visited.push({ ...loc })
-		const [ro, co] = charToDir(loc.char)
-		const [nr, nc] = [loc.r + ro, loc.c + co]
-		const inFront = world[nr]?.[nc]
-		if (inFront === '#') {
-			loc.char = chars[(chars.indexOf(loc.char) + 1) % chars.length]
-
-		} else {
-			loc.r = nr;
-			loc.c = nc;
-		}
-	}*/
-
-
-	let obs = new Set<string>()
-
-	visited = []
-	loc = { ...startingLoc }
-	while (loc.r >= 0 && loc.r < world.length && loc.c >= 0 && loc.c < world[0].length) {
-		visited.push({ ...loc })
-		const [ro, co] = charToDir(loc.char)
-		const [nr, nc] = [loc.r + ro, loc.c + co]
-		const inFront = world[nr]?.[nc]
-
-		if (inFront === '.') {
-			//const nextChar = chars[(chars.indexOf(loc.char) + 1) % chars.length]
-			/*const pathingLinesCanJoin = pathingLines.find(pl => {
-				if (pl.char !== nextChar) return false;
-				if (pl.from.r === pl.to.r && loc.r === pl.from.r) {
-					return isBetween(pl.from.c, pl.to.c, loc.c)
-				} else if (pl.from.c === pl.to.c && loc.c === pl.from.c) {
-					return isBetween(pl.from.r, pl.to.r, loc.r)
-				}
-				return false
-			})
-			if (pathingLinesCanJoin) {*/
-			obs.add(JSON.stringify({ r: nr, c: nc }))
-			//}
-		}
-
-		if (inFront === '#') {
-			const nextChar = chars[(chars.indexOf(loc.char) + 1) % chars.length]
-			loc.char = nextChar
-			//addPathingLine()
-		} else {
-			loc.r = nr;
-			loc.c = nc;
-		}
-	}
-
-	obs = [...obs].map(s => JSON.parse(s))
-
-	let loopCount = 0
-	for (const [i, { r, c }] of obs.entries()) {
+	let loopsFound = 0
+	for (const { r, c } of possibleObstructions) {
 		world[r][c] = '#'
-		if (navTheWorldEndsInLoop({ ...startingLoc }, world)) {
-			loopCount++
-		}
+		loopsFound += +pathfindRobot({ ...loc }, world).loop
 		world[r][c] = '.'
 	}
-	return loopCount
+	return loopsFound
 }
 
 
@@ -204,5 +114,5 @@ function solveTwo(data: string): any {
 #.........
 ......#...
 `.trim()), 6);
-	//console.log(solveTwo(data));
+	console.log(solveTwo(data));
 })();
