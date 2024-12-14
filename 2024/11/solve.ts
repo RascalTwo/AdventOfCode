b954fb903365435faa9eba3b39230020d6809bb4
Rascal Two <therealrascaltwo@gmail.com>
2024-12-14 12:52:04 -0500

    Optimize 2024 Day 11 Typescript Solution

--- 2024/11/solve.ts
+++ 2024/11/solve.ts
@@ -2,38 +2,6 @@ const fs = require('fs');
 const assert = require('assert');
 
 
-
-function solveOne(data: string): any {
-	const stones = data.split(' ').map(Number);
-	for (let _ = 0; _ < 25; _++) {
-		for (let s = 0; s < stones.length; s++) {
-			const stone = stones[s]
-			const stoneStr = stone.toString()
-			if (stone === 0) {
-				stones[s] = 1
-			}
-			else if (stoneStr.length % 2 === 0) {
-				const left = stoneStr.slice(0, stoneStr.length / 2)
-				const right = stoneStr.slice(stoneStr.length / 2)
-				stones.splice(s, 1, +left, +right)
-				s++
-			}
-			else {
-				stones[s] *= 2024
-			}
-		}
-	}
-	return stones.length
-}
-
-
-(() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
-	assert.deepStrictEqual(solveOne(`125 17`), 55312);
-	console.log(solveOne(data));
-})();
-
-
 function changeStone(stone: string): number[] {
 	if (+stone == 0) {
 		return [1]
@@ -48,9 +16,9 @@ function changeStone(stone: string): number[] {
 	}
 }
 
-function solveTwo(data: string): any {
+function solve(data: string, blinks: number): any {
 	let stoneCounts = data.split(' ').map(Number).reduce((counts, stone) => ({ ...counts, [stone]: (counts[stone]) ?? 0 + 1 }), {} as Record<number, number>)
-	for (let _ = 0; _ < 75; _++) {
+	for (let _ = 0; _ < blinks; _++) {
 		const newCounts: Record<number, number> = {}
 		for (const [stone, count] of Object.entries(stoneCounts)) {
 			for (const newStone of changeStone(stone)) {
@@ -60,6 +28,23 @@ function solveTwo(data: string): any {
 		stoneCounts = newCounts
 	}
 	return Object.values(stoneCounts).reduce((a, b) => a + b, 0)
+}
+
+function solveOne(data: string): any {
+	return solve(data, 25)
+}
+
+
+(() => {
+	const data = fs.readFileSync(__dirname + '/input.in').toString();
+	assert.deepStrictEqual(solveOne(`125 17`), 55312);
+	console.log(solveOne(data));
+})();
+
+
+
+function solveTwo(data: string): any {
+	return solve(data, 75)
 }
 
 
