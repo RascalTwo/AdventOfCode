d0bfe8761b9929a763b99616c75421bbefdcd4bb
Rascal Two <therealrascaltwo@gmail.com>
2025-12-03 23:41:41 -0600

    Optimize 2025 Day 04 Typescript Solution

--- 2025/04/solve.ts
+++ 2025/04/solve.ts
@@ -1,36 +1,47 @@
 const fs = require('fs');
 const assert = require('assert');
 
-
-
-function solveOne(data: string): any{
-	const world = data.trim().split('\n').map(r => r.split(''));
-	const found = []
-	let accessible = 0
-	for (let r = 0; r < world.length; r++){
-		for (let c = 0; c < world[0].length; c++){
-			let howManyRollsAround = 0;
-			if (world[r][c] !== '@') continue
-			for (let ro = -1; ro <= 1; ro++){
-				for (let co = -1; co <= 1; co++){
-					if (ro === 0 && co === 0) continue;
-					const [nr, nc] = [r + ro, c + co];
-					const value = world[nr]?.[nc]
-					if (value === '@'){
-						howManyRollsAround++
-					}
-				}
-			}
-			if (howManyRollsAround < 4) {
-				accessible++
-				found.push([r, c]);
+function countAdjacentCharacters(world: string[][], r: number, c: number): Record<string, number>{
+	const counts: Record<string, number> = {};
+	for (let ro = -1; ro <= 1; ro++){
+		for (let co = -1; co <= 1; co++){
+			if (ro === 0 && co === 0) continue;
+			const value = world[r + ro]?.[c + co];
+			if (value !== undefined){
+				counts[value] = (counts[value] ?? 0) + 1
 			}
 		}
 	}
-	for (const [r, c] of found){
-		world[r][c] = 'x'
+	return counts;
+}
+
+function solve(data: string, processRepetition: number): any{
+	const world = data.trim().split('\n').map(r => r.split(''));
+
+	let rollsRemoved = 0
+	for (let _ = 0; _ < processRepetition; _++){
+		const accessibleRolls = []
+		for (let r = 0; r < world.length; r++){
+			for (let c = 0; c < world[0].length; c++){
+				if (world[r][c] === '@' && (countAdjacentCharacters(world, r, c)['@'] ?? 0) < 4){
+					accessibleRolls.push([r, c]);
+				}
+			}
+		}
+
+		for (const [r, c] of accessibleRolls){
+			world[r][c] = '.'
+		}
+		rollsRemoved += accessibleRolls.length
+
+		if (!accessibleRolls.length) break
 	}
-	return accessible
+	return rollsRemoved
+}
+
+
+function solveOne(data: string): any{
+	return solve(data, 1);
 }
 
 
@@ -52,36 +63,7 @@ function solveOne(data: string): any{
 
 
 function solveTwo(data: string): any{
-	const world = data.trim().split('\n').map(r => r.split(''));
-	let accessible = 0
-	while (true){
-		const found = []
-		for (let r = 0; r < world.length; r++){
-			for (let c = 0; c < world[0].length; c++){
-				let howManyRollsAround = 0;
-				if (world[r][c] !== '@') continue
-				for (let ro = -1; ro <= 1; ro++){
-					for (let co = -1; co <= 1; co++){
-						if (ro === 0 && co === 0) continue;
-						const [nr, nc] = [r + ro, c + co];
-						const value = world[nr]?.[nc]
-						if (value === '@'){
-							howManyRollsAround++
-						}
-					}
-				}
-				if (howManyRollsAround < 4) {
-					accessible++
-					found.push([r, c]);
-				}
-			}
-		}
-		for (const [r, c] of found){
-			world[r][c] = '.'
-		}
-		if (!found.length) break
-	}
-	return accessible
+	return solve(data, Number.MAX_SAFE_INTEGER)
 }
 
 
