5ca7bca92ce7fc15247bbd84a4a9999fc6e78456
Rascal Two <therealrascaltwo@gmail.com>
2025-12-07 21:11:03 -0600

    Optimize 2025 Day 06 Typescript Solution

--- 2025/06/solve.ts
+++ 2025/06/solve.ts
@@ -1,27 +1,57 @@
-const fs = require('fs');
-const assert = require('assert');
+const fs = require("fs");
+const assert = require("assert");
 
-
-
-function solveOne(data: string): any {
-	const things = data.split('\n').map(line => line.trim().split(/\s+/))
-	let total = 0
-	for (let c = 0; c < things[0].length; c++) {
-		const row = [];
-		for (let r = 0; r < things.length; r++) {
-			row.push(things[r][c])
+function calculateColumnWidths(operators: string) {
+	const columnWidths = [];
+	let currentWidth = 0;
+	for (const characters of operators.slice(1) + " !") {
+		if (characters === " ") {
+			currentWidth++;
+		} else {
+			columnWidths.push(currentWidth);
+			currentWidth = 0;
 		}
-		const op = row.splice(-1)[0]
-		const expr = row.join(op)
-		const res = eval(expr)
-		total += res
 	}
-	return total
+	return columnWidths;
 }
 
+function parseProblems(data: string) {
+	const rawLines = data.split("\n")
+	return rawLines.map(
+		(line) =>
+			calculateColumnWidths(rawLines.at(-1)!).reduce(
+				({ columns, widthsSoFar }, width) => ({
+					columns: [...columns, line.slice(widthsSoFar, widthsSoFar + width)],
+					widthsSoFar: widthsSoFar + width + 1,
+				}),
+				{
+					columns: [] as string[],
+					widthsSoFar: 0,
+				}
+			).columns
+	);
+}
+
+function solve(
+	data: string,
+	generateNumbersAndOperator: (column: number, lines: string[][]) => string[]
+) {
+	const lines = parseProblems(data);
+	let total = 0;
+	for (let c = 0; c < lines[0].length; c++) {
+		const column = generateNumbersAndOperator(c, lines);
+		const operator = column.splice(-1)[0];
+		total += eval(column.join(" " + operator + " "));
+	}
+	return total;
+}
+
+function solveOne(data: string) {
+	return solve(data, (c, lines) => lines.map((row) => row[c]));
+}
 
 (() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
+	const data = fs.readFileSync(__dirname + "/input.in").toString();
 	assert.deepStrictEqual(solveOne(`123 328  51 64 
  45 64  387 23 
   6 98  215 314
@@ -29,60 +59,19 @@ function solveOne(data: string): any {
 	console.log(solveOne(data));
 })();
 
-
 function solveTwo(data: string): any {
-	//const things = data.split('\n').map(line => line.trim().split(/\s+/))
-	let colWidths = [];
-	const ops = data.split('\n').at(-1)!
-	let currWidth = 0
-	for (let i = 0; i < ops.length; i++) {
-		if (ops[i] === ' ') currWidth++
-		else {
-			colWidths.push(currWidth)
-			currWidth = 0
+	return solve(data, (c, lines) => {
+		const columns = lines.map((row) => row[c])
+		const numbers = [];
+		for (let c = columns[0].length - 1; c >= 0; c--) {
+			numbers.push(columns.slice(0, -1).map(row => row[c]).join('').trim())
 		}
-	}
-	if (currWidth) colWidths.push(currWidth + 1)
-	colWidths.splice(0, 1);
-	const newThings = [];
-	for (let line of data.split('\n')) {
-		const chars = line.split('')
-		const cols = [];
-		for (let width of colWidths) {
-			cols.push(chars.splice(0, width).join(''))
-			chars.splice(0, 1)
-		}
-		newThings.push(cols)
-	}
-	let total = 0
-	for (let c = 0; c < newThings[0].length; c++) {
-		const row = [];
-		for (let r = 0; r < newThings.length; r++) {
-			row.push(newThings[r][c])
-		}
-		const op = row.splice(-1)[0]
-		const longestOfRow = row.reduce((a, b) => a.length > b.length ? a : b, '').length
-		//const paddedRow = row.map(s => s.padStart(longestOfRow, ' '))
-		//paddedRow
-		const nums = []
-		for (let rc = row[0].length - 1; rc >= 0; rc--) {
-			let newNum = ''
-			for (let rr = 0; rr < row.length; rr++) {
-				newNum += row[rr][rc]
-			}
-			nums.push(newNum.trim())
-		}
-		const expr = nums.join(op)
-		expr
-		const res = eval(expr)
-		total += res
-	}
-	return total
+		return [...numbers, columns.at(-1)!];
+	});
 }
 
-
 (() => {
-	const data = fs.readFileSync(__dirname + '/input.in').toString();
+	const data = fs.readFileSync(__dirname + "/input.in").toString();
 	assert.deepStrictEqual(solveTwo(`123 328  51 64 
  45 64  387 23 
   6 98  215 314
@@ -90,4 +79,4 @@ function solveTwo(data: string): any {
 	console.log(solveTwo(data));
 })();
 
-export { }
+export { };
