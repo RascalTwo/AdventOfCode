6026bc01327ddcd602a14b5aafdfc76a9ea5aede
Rascal Two <therealrascaltwo@gmail.com>
2025-12-08 22:10:18 -0600

    Optimize 2025 Day 08 Typescript Solution

--- 2025/08/solve.ts
+++ 2025/08/solve.ts
@@ -1,62 +1,79 @@
 const fs = require('fs');
 const assert = require('assert');
 
-function euclideanDistance3D(point1, point2) {
-  const dx = point2.x - point1.x;
-  const dy = point2.y - point1.y;
-  const dz = point2.z - point1.z;
+type Point = Record<'x' | 'y' | 'z', number>
 
-  return Math.sqrt((dx * dx) + (dy * dy) + (dz * dz));
+function euclideanDistance3D(point1: Point, point2: Point) {
+	const dx = point2.x - point1.x;
+	const dy = point2.y - point1.y;
+	const dz = point2.z - point1.z;
+
+	return Math.sqrt((dx * dx) + (dy * dy) + (dz * dz));
 }
-function solveOne(data: string, connectionCount: number): any {
-	const boxes = data.split('\n').map(l => {
+
+const serialize = ({ x, y, z }: Point) => `${x},${y},${z}`
+
+const setsShareAValue = <T>(a: Set<T>, b: Set<T>) => {
+	for (const aValue of a) {
+		if (b.has(aValue)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function solve(data: string, connectionsToMake: number, generateAnswer: (state: { a: Point, b: Point, groups: Set<string>[] }) => number): any {
+	const points = data.split('\n').map(l => {
 		const [x, y, z] = l.split(',').map(Number)
-		return { x, y, z }
+		return { x, y, z } as Point
 	})
-	const possibleConnections = [];
-	for (const [ai, a] of boxes.entries()) {
-		for (const b of boxes.slice(ai + 1)) {
+
+	const possibleConnections: { a: Point, b: Point, distance: number }[] = [];
+	for (const [ai, a] of points.entries()) {
+		for (const b of points.slice(ai + 1)) {
 			possibleConnections.push({ a, b, distance: euclideanDistance3D(a, b) })
 		}
 	}
-	possibleConnections.sort((one, two) => {
-		return one.distance - two.distance
-	})
-	const connections: Record<string, string[]> = {};
-	for (let i = 0; i < connectionCount; i++) {
-		const { a, b }  = possibleConnections[i];
-		connections[JSON.stringify(a)] = [...(connections[JSON.stringify(a)] ?? []), JSON.stringify(b)]
-	}
+	possibleConnections.sort((one, two) => one.distance - two.distance);
+
 	const groups: Set<string>[] = [];
-	for (const key in connections){
-		let group = groups.find(g => g.has(key))
+	for (let i = 0; i < connectionsToMake; i++) {
+		const { a, b } = possibleConnections[i];
+		const serializedA = serialize(a);
+
+		let group = groups.find(g => g.has(serializedA))
 		if (!group) {
-			group = new Set([key]);
+			group = new Set([serializedA]);
 			groups.push(group)
 		}
-		for (const conn of connections[key]){
-			group.add(conn)
-		}
-	}
-	while (true){
-		let merged = false
-		for (const a of groups){
-			for (const b of groups){
-				if (a === b) continue;
-				if ([...a].some(ai => b.has(ai))){
-					for (const bi of b){
-						a.add(bi)
-					}
-					groups.splice(groups.indexOf(b), 1)
-					merged = true
-					break
+		group.add(serialize(b))
+
+		outerLoop:
+		for (let ai = 0; ai < groups.length; ai++) {
+			const a = groups[ai]
+			for (let bi = ai + 1; bi < groups.length; bi++) {
+				const b = groups[bi];
+
+				if (setsShareAValue(a, b)) {
+					for (const point of b) a.add(point)
+					groups.splice(bi, 1)
+					break outerLoop;
 				}
 			}
 		}
-		if (!merged) break
+
+		if (groups.length === 1 && groups[0].size === points.length) {
+			return generateAnswer({ a, b, groups })
+		}
+		if (i === connectionsToMake - 1) {
+			return generateAnswer({ a, b, groups })
+		}
 	}
-	groups.sort((a, b) => b.size - a.size)
-	return groups.slice(0, 3).map(g => g.size).reduce((a, b) => a * b, 1)
+}
+
+
+function solveOne(data: string, connectionCount: number): any {
+	return solve(data, connectionCount, ({ groups }) => groups.sort((a, b) => b.size - a.size).slice(0, 3).map(g => g.size).reduce((a, b) => a * b, 1))
 }
 
 
@@ -87,60 +104,7 @@ function solveOne(data: string, connectionCount: number): any {
 
 
 function solveTwo(data: string): any {
-	const boxes = data.split('\n').map(l => {
-		const [x, y, z] = l.split(',').map(Number)
-		return { x, y, z }
-	})
-	const possibleConnections = [];
-	for (const [ai, a] of boxes.entries()) {
-		for (const b of boxes.slice(ai + 1)) {
-			possibleConnections.push({ a, b, distance: euclideanDistance3D(a, b) })
-		}
-	}
-	possibleConnections.sort((one, two) => {
-		return one.distance - two.distance
-	})
-	const connections: Record<string, string[]> = {};
-	let i = 0
-	while (true) {
-		const { a, b }  = possibleConnections[i];
-		i++;
-		connections[JSON.stringify(a)] = [...(connections[JSON.stringify(a)] ?? []), JSON.stringify(b)]
-		const groups: Set<string>[] = [];
-		for (const key in connections){
-			let group = groups.find(g => g.has(key))
-			if (!group) {
-				group = new Set([key]);
-				groups.push(group)
-			}
-			for (const conn of connections[key]){
-				group.add(conn)
-			}
-		}
-		while (true){
-			let merged = false
-			for (const a of groups){
-				for (const b of groups){
-					if (a === b) continue;
-					if ([...a].some(ai => b.has(ai))){
-						for (const bi of b){
-							a.add(bi)
-						}
-						groups.splice(groups.indexOf(b), 1)
-						merged = true
-						break
-					}
-				}
-			}
-			if (!merged) break
-		}
-		groups.sort((a, b) => b.size - a.size)
-		if (groups.length === 1 && groups[0].size === boxes.length) {
-			a
-			b
-			return a.x * b.x;
-		}
-	}
+	return solve(data, Infinity, ({ a, b }) => a.x * b.x)
 }
 
 
